6b83329737d68a556cf8408cea1e255555fa2c18
Jason Gilbertson <jagilber@microsoft.com>
2017-08-31 02:00:50 -0400

    issue61v2 fix netbios change new-object Management.Automation.PSCredential from domain\user to user@domain. rezipped configuration.zip

--- Scripts/deploy-rds-templates.ps1
+++ Scripts/deploy-rds-templates.ps1
@@ -236,7 +236,7 @@
 #>
 [CMDLETBINDING()]
 param(
-    [string]$random = ((get-random).ToString().Substring(0, 5)), # positional requirement
+    [string]$random = ((get-random).ToString().Substring(0, 9)), # positional requirement
     [string]$adminUserName = "cloudadmin",
     [string]$adminPassword = "Password$($random)!", 
     [string]$applicationId, 
