0757acc6f3d17de67c31e07aedfced053c0921fd
viswanadham <sreenatha.kk@gmail.com>
2018-06-07 10:05:43 +0530

    modified script in Tenantand and hostpool creation

--- rdmi-peopletech/Tenant & Hostpool Creation/Scripts/Script.ps1
+++ rdmi-peopletech/Tenant & Hostpool Creation/Scripts/Script.ps1
@@ -45,6 +45,17 @@ Param(
  
 )
 
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+Disable-ieESC
+
 
 Invoke-WebRequest -Uri $fileURI -OutFile "C:\PSModules.zip"
 New-Item -Path "C:\PSModules" -ItemType directory -Force -ErrorAction SilentlyContinue
