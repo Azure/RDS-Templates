e166f18a857816026b83a6afe173334ccddcb032
Paulo Marques <paulomarquesc@users.noreply.github.com>
2018-12-18 15:47:22 -0700

    Changes applied in private repo, reflected in pub repo (#84)
    * Changes applied in private repo, reflected in pub repo
    
    * Removed extra comma
    
    * Removing Set-RdsSessionHost since it is not necessary at this moment
    
    * Replaced TryAddSessionHost with a Class that can add or retrieve.
    
    * Remove unecessary comment

--- wvd-templates/Create and provision WVD host pool/Scripts/Script.ps1
+++ wvd-templates/Create and provision WVD host pool/Scripts/Script.ps1
@@ -11,8 +11,6 @@ The supported Operating Systems Windows Server 2016.
 Readers
 
 #>
-
-
 param(
     [Parameter(mandatory = $true)]
     [string]$RDBrokerURL,
@@ -29,15 +27,11 @@ param(
     [Parameter(mandatory = $false)]
     [string]$Description,
 
-
     [Parameter(mandatory = $false)]
     [string]$FriendlyName,
 
     [Parameter(mandatory = $true)]
     [string]$Hours,
-
-    [Parameter(mandatory = $true)]
-    [string]$FileURI,
 
     [Parameter(mandatory = $true)]
     [string]$TenantAdminUPN,
@@ -58,34 +52,144 @@ param(
     [string]$isServicePrincipal = "False",
 
     [Parameter(Mandatory = $false)]
-    [string]$AadTenantId
+    [string]$AadTenantId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$ActivationKey
+
 )
 
-
-
-function Write-Log { 
+function Write-Log
+{ 
     [CmdletBinding()] 
-    param ( 
+    param
+    ( 
         [Parameter(Mandatory = $false)] 
         [string]$Message,
         [Parameter(Mandatory = $false)] 
         [string]$Error 
     ) 
      
-    try { 
+    try
+    { 
         $DateTime = Get-Date -Format ‘MM-dd-yy HH:mm:ss’ 
         $Invocation = "$($MyInvocation.MyCommand.Source):$($MyInvocation.ScriptLineNumber)" 
-        if ($Message) {
+        if ($Message)
+        {
             Add-Content -Value "$DateTime - $Invocation - $Message" -Path "$([environment]::GetEnvironmentVariable('TEMP', 'Machine'))\ScriptLog.log" 
         }
-        else {
+        else
+        {
             Add-Content -Value "$DateTime - $Invocation - $Error" -Path "$([environment]::GetEnvironmentVariable('TEMP', 'Machine'))\ScriptLog.log" 
         }
     } 
-    catch { 
+    catch
+    { 
         Write-Error $_.Exception.Message 
     } 
 }
+
+function ActivateWvdSku
+{
+    param
+    (
+        [Parameter(Mandatory = $true)] 
+        [string]$ActivationKey
+    )
+
+    cscript c:\windows\system32\slmgr.vbs /ipk $ActivationKey
+    dism /online /Enable-Feature /FeatureName:AppServerClient /NoRestart /Quiet
+}
+
+class PsRdsSessionHost
+{
+    [string]$TenantName = [string]::Empty
+    [string]$HostPoolName = [string]::Empty
+    [string]$SessionHostName = [string]::Empty
+    [int]$TimeoutInMin=900 
+
+    PsRdsSessionHost() {}
+
+    PsRdsSessionHost([string]$TenantName, [string]$HostPoolName, [string]$SessionHostName) {
+        $this.TenantName = $TenantName
+        $this.HostPoolName = $HostPoolName
+        $this.SessionHostName = $SessionHostName
+    }
+
+    PsRdsSessionHost([string]$TenantName, [string]$HostPoolName, [string]$SessionHostName, [int]$TimeoutInMin) {
+        
+        if ($TimeoutInMin -gt 1800)
+        {
+            throw "TimeoutInMin is too high, maximum value is 1800"
+        }
+
+        $this.TenantName = $TenantName
+        $this.HostPoolName = $HostPoolName
+        $this.SessionHostName = $SessionHostName
+        $this.TimeoutInMin = $TimeoutInMin
+    }
+
+    hidden [object] _trySessionHost([string]$operation)
+    {
+        if ($operation -ne "get" -and $operation -ne "set")
+        {
+            throw "PsRdsSessionHost: Invalid operation: $operation. Valid Operations are get or set"
+        }
+
+        $specificToSet=@{$true = "-AllowNewSession `$true"; $false = ""}[$operation -eq "set"]
+        $commandToExecute="$operation-RdsSessionHost -TenantName `$this.TenantName -HostPoolName `$this.HostPoolName -Name `$this.SessionHostName -ErrorAction SilentlyContinue $specificToSet"
+
+        $sessionHost = (Invoke-Expression $commandToExecute )
+
+        $StartTime = Get-Date
+        while ($sessionHost -eq $null)
+        {
+            Start-Sleep (60..120 | Get-Random)
+            Write-Output "PsRdsSessionHost: Retrying Add SessionHost..."
+            $sessionHost = (Invoke-Expression $commandToExecute)
+    
+            if ((get-date).Subtract($StartTime).Minutes -gt $this.TimeoutInMin)
+            {
+                if ($sessionHost -eq $null)
+                {
+                    Write-Output "PsRdsSessionHost: An error ocurred while adding session host:`nSessionHost:$this.SessionHostname`nHostPoolName:$this.HostPoolNmae`nTenantName:$this.TenantName`nError Message: $($error[0] | Out-String)"
+                    return $null
+                }
+            }
+        }
+
+        return $sessionHost
+    }
+
+    [object] SetSessionHost() {
+
+        if ([string]::IsNullOrEmpty($this.TenantName) -or [string]::IsNullOrEmpty($this.HostPoolName) -or [string]::IsNullOrEmpty($this.HostPoolName))
+        {
+            return $null
+        }
+        else
+        {
+            
+            return ($this._trySessionHost("set"))
+        }
+    }
+    
+    [object] GetSessionHost() {
+
+        if ([string]::IsNullOrEmpty($this.TenantName) -or [string]::IsNullOrEmpty($this.HostPoolName) -or [string]::IsNullOrEmpty($this.HostPoolName))
+        {
+            return $null
+        }
+        else
+        {
+            return ($this._trySessionHost("get"))
+        }
+    }
+}
+
+
+# Setting ErrorActionPreference to stop script execution when error occurs
+$ErrorActionPreference = "Stop"
 
 # Setting to Tls12 due to Azure web app security requirements
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -93,36 +197,38 @@ function Write-Log {
 $DeployAgentLocation = "C:\DeployAgent"
 $rdshIs1809OrLaterBool = ($rdshIs1809OrLater -eq "True")
 
-# Downloading the DeployAgent zip file to rdsh vm
-Invoke-WebRequest -Uri $fileURI -OutFile "C:\DeployAgent.zip"
-Write-Log -Message "Downloaded DeployAgent.zip into this location C:\"
+Write-Log -Message "Creating a folder inside rdsh vm for extracting deployagent zip file"
+if (Test-Path $DeployAgentLocation)
+{
+    Remove-Item -Path $DeployAgentLocation -Force -Confirm:$false -Recurse
+}
 
-# Creating a folder inside rdsh vm for extracting deployagent zip file
-New-Item -Path "$DeployAgentLocation" -ItemType directory -Force -ErrorAction SilentlyContinue
-Write-Log -Message "Created a new folder 'DeployAgent' inside VM"
-Expand-Archive "C:\DeployAgent.zip" -DestinationPath "$DeployAgentLocation" -ErrorAction SilentlyContinue
-Write-Log -Message "Extracted the 'Deployagent.zip' file into '$DeployAgentLocation' folder inside VM"
+New-Item -Path "$DeployAgentLocation" -ItemType directory -Force 
+
+Write-Log -Message "Extracting 'Deployagent.zip' file into '$DeployAgentLocation' folder inside VM"
+Expand-Archive ".\Scripts\DeployAgent.zip" -DestinationPath "$DeployAgentLocation" 
+
+Write-Log -Message "Changing current folder to Deployagent folder: $DeployAgentLocation"
 Set-Location "$DeployAgentLocation"
-Write-Log -Message "Setting up the location of Deployagent folder"
 
 # Checking if RDInfragent is registered or not in rdsh vm
 $CheckRegistry = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent" -ErrorAction SilentlyContinue
 
 Write-Log -Message "Checking whether VM was Registered with RDInfraAgent"
 
-if ($CheckRegistry) {
+if ($CheckRegistry)
+{
     Write-Log -Message "VM was already registered with RDInfraAgent, script execution was stopped"
 }
-else {
-    Write-Log -Message "VM was not registered with RDInfraAgent, script is executing"
-}
+else
+{
+    Write-Log -Message "VM not registered with RDInfraAgent, script execution will continue"
 
-
-if (!$CheckRegistry) {
-    
     # Importing WVD PowerShell module
     Import-Module .\PowershellModules\Microsoft.RDInfra.RDPowershell.dll
+
     Write-Log -Message "Imported RDMI PowerShell modules successfully"
+    
     $Securepass = ConvertTo-SecureString -String $TenantAdminPassword -AsPlainText -Force
     $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($TenantAdminUPN, $Securepass)
     $AdminSecurepass = ConvertTo-SecureString -String $localAdminPassword -AsPlainText -Force
@@ -130,85 +236,117 @@ if (!$CheckRegistry) {
 
     # Getting fqdn of rdsh vm
     $SessionHostName = (Get-WmiObject win32_computersystem).DNSHostName + "." + (Get-WmiObject win32_computersystem).Domain
+
     Write-Log  -Message "Getting fully qualified domain name of RDSH VM: $SessionHostName"
 
     # Authenticating to WVD
-    if ($isServicePrincipal -eq "True"){
+    if ($isServicePrincipal -eq "True")
+    {
         $authentication = Add-RdsAccount -DeploymentUrl $RDBrokerURL -Credential $Credentials -ServicePrincipal -TenantId $AadTenantId 
-    } else {
+    }
+    else
+    {
         $authentication = Add-RdsAccount -DeploymentUrl $RDBrokerURL -Credential $Credentials
     }
     $obj = $authentication | Out-String
 
-    if ($authentication) {
-        Write-Log -Message "RDMI Authentication successfully Done. Result: `
-    $obj"  
+    if ($authentication)
+    {
+        Write-Log -Message "RDMI Authentication successfully Done. Result:`n$obj"  
     }
-    else {
-        Write-Log -Error "RDMI Authentication Failed, Error: `
-    $obj"
-    
+    else
+    {
+        Write-Log -Error "RDMI Authentication Failed, Error:`n$obj"
+        throw "RDMI Authentication Failed, Error:`n$obj"
     }
 
     # Set context to the appropriate tenant group
     Write-Log "Running switching to the $definedTenantGroupName context"
     Set-RdsContext -TenantGroupName $definedTenantGroupName
-    try {
-        $tenants = Get-RdsTenant
-        if( !$tenants ) {
+    try
+    {
+        $tenants = Get-RdsTenant -Name $TenantName
+        if(!$tenants)
+        {
             Write-Log "No tenants exist or you do not have proper access."
         }
-    } catch {
-        Write-Log -Message ""
+    }
+    catch
+    {
+        Write-Log -Message $_
+        throw $_
     }
 
     # Checking if host pool exists. If not, create a new one with the given HostPoolName
-    $HPName = Get-RdsHostPool -TenantName $TenantName -Name $HostPoolName -ErrorAction SilentlyContinue
     Write-Log -Message "Checking Hostpool exists inside the Tenant"
-    if ($HPName) {
+    $HPName = Get-RdsHostPool -TenantName $TenantName -Name $HostPoolName -ErrorAction SilentlyContinue
+    if ($HPName)
+    {
         Write-log -Message "Hostpool exists inside tenant: $TenantName"
     }
-    else {
+    else
+    {
         $HPName = New-RdsHostPool -TenantName $TenantName -Name $HostPoolName -Description $Description -FriendlyName $FriendlyName
-
         $HName = $HPName.name | Out-String -Stream
         Write-Log -Message "Successfully created new Hostpool: $HName"
     }
 
     # Setting UseReverseConnect property to true
     Write-Log -Message "Checking Hostpool UseResversconnect is true or false"
-    if ($HPName.UseReverseConnect -eq $False) {
+    if ($HPName.UseReverseConnect -eq $False)
+    {
         Write-Log -Message "UseReverseConnect is false, it will be changed to true"
         Set-RdsHostPool -TenantName $TenantName -Name $HostPoolName -UseReverseConnect $true
     }
-    else {
+    else
+    {
         Write-Log -Message "Hostpool UseReverseConnect already enabled as true"
     }
     
     # Creating registration token
-    $Registered = $null
-    try {
-        $Registered = Export-RdsRegistrationInfo -TenantName $TenantName -HostPoolName $HostPoolName
-        if (!$Registered) {
-            $Registered = New-RdsRegistrationInfo -TenantName $TenantName -HostPoolName $HostPoolName -ExpirationHours $Hours
-            Write-Log -Message "Created new Rds RegistrationInfo into variable 'Registered': $Registered"
-        } else {
-            Write-Log -Message "Exported Rds RegistrationInfo into variable 'Registered': $Registered"
-        }
-    } catch {
+    $Registered = Export-RdsRegistrationInfo -TenantName $TenantName -HostPoolName $HostPoolName -ErrorAction SilentlyContinue
+    if (!$Registered)
+    {
         $Registered = New-RdsRegistrationInfo -TenantName $TenantName -HostPoolName $HostPoolName -ExpirationHours $Hours
-        Write-Log -Message "Created new Rds RegistrationInfo into variable 'Registered': $Registered"
+        $obj =  $Registered | Out-String
+        Write-Log -Message "Created new Rds RegistrationInfo into variable 'Registered': $obj"
+    }
+    else
+    {
+        $obj =  $Registered | Out-String
+        Write-Log -Message "Exported Rds RegistrationInfo into variable 'Registered': $obj"
     }
 
     # Executing DeployAgent psl file in rdsh vm and add to hostpool
     Write-Log "AgentInstaller is $DeployAgentLocation\RDAgentBootLoaderInstall, InfraInstaller is $DeployAgentLocation\RDInfraAgentInstall, SxS is $DeployAgentLocation\RDInfraSxSStackInstall"
-    $DAgentInstall = .\DeployAgent.ps1 -ComputerName $SessionHostName -AgentBootServiceInstallerFolder "$DeployAgentLocation\RDAgentBootLoaderInstall" -AgentInstallerFolder "$DeployAgentLocation\RDInfraAgentInstall" -SxSStackInstallerFolder "$DeployAgentLocation\RDInfraSxSStackInstall" -EnableSxSStackScriptFolder "$DeployAgentLocation\EnableSxSStackScript" -AdminCredentials $adminCredentials -TenantName $TenantName -PoolName $HostPoolName -RegistrationToken $Registered.Token -StartAgent $true -rdshIs1809OrLater $rdshIs1809OrLaterBool
-    Write-Log -Message "DeployAgent Script was successfully executed and RDAgentBootLoader,RDAgent,StackSxS installed inside VM for existing hostpool: $HostPoolName `
-    $DAgentInstall"
+    $DAgentInstall = .\DeployAgent.ps1 -ComputerName $SessionHostName `
+                                       -AgentBootServiceInstallerFolder "$DeployAgentLocation\RDAgentBootLoaderInstall" `
+                                       -AgentInstallerFolder "$DeployAgentLocation\RDInfraAgentInstall" `
+                                       -SxSStackInstallerFolder "$DeployAgentLocation\RDInfraSxSStackInstall" `
+                                       -EnableSxSStackScriptFolder "$DeployAgentLocation\EnableSxSStackScript" `
+                                       -AdminCredentials $adminCredentials `
+                                       -TenantName $TenantName `
+                                       -PoolName $HostPoolName `
+                                       -RegistrationToken $Registered.Token `
+                                       -StartAgent $true `
+                                       -rdshIs1809OrLater $rdshIs1809OrLaterBool
+    
+    Write-Log -Message "DeployAgent Script was successfully executed and RDAgentBootLoader,RDAgent,StackSxS installed inside VM for existing hostpool: $HostPoolName`n$DAgentInstall"
 
-    #add rdsh vm to hostpool
-    $addRdsh = Set-RdsSessionHost -TenantName $TenantName -HostPoolName $HostPoolName -Name $SessionHostName -AllowNewSession $true
-    $rdshName = $addRdsh.name | Out-String -Stream
-    $poolName = $addRdsh.hostpoolname | Out-String -Stream
+    # Get Session Host Info
+    Write-Log -Message "Getting rdsh host $SessionHostName information"
+
+    [Microsoft.RDInfra.RDManagementData.RdMgmtSessionHost]$rdsh = ([PsRdsSessionHost]::new($TenantName,$HostPoolName,$SessionHostName)).GetSessionHost()
+    Write-Log -Message "RDSH object content: `n$($rdsh | Out-String)"
+
+    $rdshName = $rdsh.SessionHostName | Out-String -Stream
+    $poolName = $rdsh.hostpoolname | Out-String -Stream
+  
+    Write-Log -Message "Activating Windows Virtual Desktop SKU"
+    ActivateWvdSku -ActivationKey $ActivationKey
+
     Write-Log -Message "Successfully added $rdshName VM to $poolName"
+
+    Write-Log -Message "Reeboting VM"
+    Shutdown -r -t 90
 }
