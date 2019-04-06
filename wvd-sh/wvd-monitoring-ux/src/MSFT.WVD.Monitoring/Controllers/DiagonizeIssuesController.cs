fd28edfcdc2f741857d6f07628515b3d702538b9
Naresh Mangillipally <naresh.mangillipally@ptgindia.com>
2019-04-06 17:06:38 +0530

    Implemented diagoniseissuescontroller

--- wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/Controllers/DiagonizeIssuesController.cs
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/Controllers/DiagonizeIssuesController.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using MSFT.WVD.Monitoring.Common.Models;
+using MSFT.WVD.Monitoring.Models;
+using Newtonsoft.Json;
+
 namespace MSFT.WVD.Monitoring.Controllers
 {
     public class DiagonizeIssuesController : Controller
     {
+        List<ManagementActivity> managementActivities;
+        List<ConnectionActivity> connectionActivities;
+        List<FeedActivity> feedActivities;
         public async Task<IActionResult> Index()
         {
 
@@ -17,46 +23,62 @@ namespace MSFT.WVD.Monitoring.Controllers
 
         }
 
-
-        [HttpPost]
-        public async Task<ActionResult> SearchActivity()
+        public async Task<IActionResult> SearchActivity([FromBody] DaigonisepageModel data)
         {
-            if (User.Identity.IsAuthenticated)
+
+            string upn = User.Claims.First(claim => claim.Type.Contains("upn")).Value;
+            string accessToken = await HttpContext.GetTokenAsync("access_token");
+            string tenantGroupname = HttpContext.Session.Get<string>("selectedTenantGroupName");
+            string tenantname = HttpContext.Session.Get<string>("selectedTenantName");           
+            using (var client = new HttpClient())
             {
-                string accessToken = await HttpContext.GetTokenAsync("access_token");
-                IEnumerable<DiagnosticActivity> diagnosticactivities = null;
-                using (var client = new HttpClient())
+                client.BaseAddress = new Uri("https://localhost:44393/api/");
+                client.Timeout = TimeSpan.FromMinutes(30);
+                //HTTP GET
+                if (data.activitytype == "Management")
                 {
-                    client.BaseAddress = new Uri("https://localhost:44393/api/");
-                    //HTTP GET
-                    //  var responseTask = client.GetAsync("DiagnosticActivity/GetActivityDetails/?accessToken=" + accessToken + "&tenantGroup=" + tenantGroup + "&tenant=" + tenant + "&startDate=" + startTime + " &endDate=" + enddate + "&activityType=" + activitytime + "&outcome=" null);
-                    var responseTask = client.GetAsync("DiagnosticActivity/GetActivityDetails/?accessToken=" + accessToken + "&tenantGroup = Default Tenant Group & tenant = Peopletech - Tenant & startDate = 2019 - 03 - 27 & endDate = 2019 - 03 - 29 & activityType = 2 & outcome = null");
-                    responseTask.Wait();
-
-                    var result = responseTask.Result;
-                    if (result.IsSuccessStatusCode)
+                    var response = await client.GetAsync("DiagnosticActivity/GetManagementActivities/?accessToken=" + accessToken + "&tenantGroup=" + tenantGroupname + "&tenant=" + tenantname + "&startDate=" + data.startdate + "&endDate=" + data.enddate + "&activityType=" + data.activitytype + "&outcome = null");
+                    if (response.IsSuccessStatusCode)
                     {
-                        //var readTask = result.Content.ReadAsAsync<JArray>();
-                        var readTask = result.Content.ReadAsAsync<IList<RoleAssignment>>();
-                        readTask.Wait();
-
-                        diagnosticactivities = (IEnumerable<DiagnosticActivity>)readTask.Result;
-                    }
-                    else //web api sent error response 
-                    {
-                        //log response status here..
-
-                        //tenantGroups = Enumerable.Empty<TenantGroup>();
-
-                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                        var strmanagementdetails = response.Content.ReadAsStringAsync().Result;
+                        managementActivities = JsonConvert.DeserializeObject<List<ManagementActivity>>(strmanagementdetails);
+                        return View(new DaigonisepageModel()
+                        {
+                            managementActivity = managementActivities
+                        });
                     }
                 }
-                return View(diagnosticactivities);
+                else if (data.activitytype == "Connection")
+                {
+                    var response = await client.GetAsync("DiagnosticActivity/GetConnectionActivities/?accessToken=" + accessToken + "&tenantGroup=" + tenantGroupname + "&tenant=" + tenantname + "&startDate=" + data.startdate + "&endDate=" + data.enddate + "&activityType=" + data.activitytype + "&outcome = null");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var strconnectiondetails = response.Content.ReadAsStringAsync().Result;
+                        //var roleAssignments = JsonConvert.DeserializeObject(strRoleAssignments);
+                        connectionActivities = JsonConvert.DeserializeObject<List<ConnectionActivity>>(strconnectiondetails);
+                        return View(new DaigonisepageModel()
+                        {
+                            connectionActivity = connectionActivities
+                        });
+                    }
+                }
+                else if (data.activitytype == "Feed")
+                {
+                    var response = await client.GetAsync("DiagnosticActivity/GetFeedActivities/?accessToken=" + accessToken + "&tenantGroup=" + tenantGroupname + "&tenant=" + tenantname + "&startDate=" + data.startdate + "&endDate=" + data.enddate + "&activityType=" + data.activitytype + "&outcome = null");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var strfeeddetails = response.Content.ReadAsStringAsync().Result;
+                        //var roleAssignments = JsonConvert.DeserializeObject(strRoleAssignments);
+                        feedActivities = JsonConvert.DeserializeObject<List<FeedActivity>>(strfeeddetails);
+                        return View(new DaigonisepageModel()
+                        {
+                            feedActivity = feedActivities
+                        });
+                    }
+                }
             }
-            else
-            {
-                return null;
-            }
+            return View("Index", "Home");
         }
+
     }
 }
---
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/Models/DaigonisepageModel.cs
@@ -0,0 +1,23 @@
+﻿using MSFT.WVD.Monitoring.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSFT.WVD.Monitoring.Models
+{
+    public class DaigonisepageModel
+    {
+        public string upn { get; set; }
+        public DateTime startdate { get; set; }
+        public DateTime enddate { get; set; }
+        public string activitytype { get; set; }
+        public string outcome { get; set; }
+        public List<ManagementActivity> managementActivity { get; set; }
+        public List<ConnectionActivity> connectionActivity { get; set; }
+        public List<FeedActivity> feedActivity { get; set; }
+        public ActivityType activityType {get;set;}
+        public ActivityOutcome activityOutcome { get; set; }
+    }
+
+}
