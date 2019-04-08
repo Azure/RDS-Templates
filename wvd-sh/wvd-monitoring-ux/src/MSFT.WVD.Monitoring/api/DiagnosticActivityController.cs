d13694f2bb205b91d0c3252b3f490193d25764dc
Susmita Pattanaik <susmita.pattanaik@ptgindia.com>
2019-04-08 11:19:09 +0530

    Added authorization header for api call

--- wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/api/DiagnosticActivityController.cs
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/api/DiagnosticActivityController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MSFT.WVD.Monitoring.Common.BAL;
 using MSFT.WVD.Monitoring.Common.Models;
+using MSFT.WVD.Monitoring.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,22 +24,25 @@ namespace MSFT.WVD.Monitoring.api
         }
 
         [HttpGet("GetConnectionActivities")]
-        public IEnumerable<ConnectionActivity> GetConnectionActivities(string accessToken, string upn, string tenantGroupName, string tenant, DateTime startDate, DateTime endDate,  Nullable<int> outcome)
+        public IEnumerable<ConnectionActivity> GetConnectionActivities( string upn, string tenantGroupName, string tenant, string startDate, string endDate, Nullable<int> outcome)
         {
-            return diagnosticActivitityBL.GetConnectionActivities(Configuration.RDBrokerUrl, accessToken, upn, tenantGroupName, tenant, startDate, endDate, outcome);
+            string token = Request.Headers["Authorization"];
+            return diagnosticActivitityBL.GetConnectionActivities(Configuration.RDBrokerUrl, token, upn, tenantGroupName, tenant, startDate, endDate, outcome);
         }
 
 
         [HttpGet("GetManagementActivities")]
-        public IEnumerable<ManagementActivity> GetManagementActivities(string accessToken, string upn, string tenantGroupName, string tenant, DateTime startDate, DateTime endDate,  Nullable<int> outcome)
+        public IEnumerable<ManagementActivity> GetManagementActivities( string upn, string tenantGroupName, string tenant, string startDate, string endDate,  Nullable<int> outcome)
         {
-            return diagnosticActivitityBL.GetManagementActivities(Configuration.RDBrokerUrl, accessToken, upn, tenantGroupName, tenant, startDate, endDate,  outcome);
+            string token = Request.Headers["Authorization"];
+            return diagnosticActivitityBL.GetManagementActivities(Configuration.RDBrokerUrl, token, upn, tenantGroupName, tenant, startDate, endDate,  outcome);
         }
 
         [HttpGet("GetFeedActivities")]
-        public IEnumerable<FeedActivity> GetFeedActivities(string accessToken, string upn, string tenantGroupName, string tenant, DateTime startDate, DateTime endDate,  Nullable<int> outcome)
+        public IEnumerable<FeedActivity> GetFeedActivities( string upn, string tenantGroupName, string tenant, string startDate, string endDate,  Nullable<int> outcome)
         {
-            return diagnosticActivitityBL.GetFeedActivities(Configuration.RDBrokerUrl, accessToken, upn, tenantGroupName, tenant, startDate, endDate,  outcome);
+            string token = Request.Headers["Authorization"];
+            return diagnosticActivitityBL.GetFeedActivities(Configuration.RDBrokerUrl, token, upn, tenantGroupName, tenant, startDate, endDate,  outcome);
         }
     }
 }
