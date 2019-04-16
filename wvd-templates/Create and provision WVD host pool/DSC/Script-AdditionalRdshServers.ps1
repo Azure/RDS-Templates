0495446ec6ff3369a71fc40d001e3d907a8aaa05
David Tran <datr@microsoft.com>
2019-04-16 14:16:48 -0700

    remove more stuff

--- wvd-templates/Create and provision WVD host pool/DSC/Script-AdditionalRdshServers.ps1
+++ wvd-templates/Create and provision WVD host pool/DSC/Script-AdditionalRdshServers.ps1
@@ -75,72 +75,6 @@ else
 {
     Write-Log -Message "VM not registered with RDInfraAgent, script execution will continue"
 
-    # Importing WVD PowerShell module
-    Import-Module .\PowershellModules\Microsoft.RDInfra.RDPowershell.dll
-
-    Write-Log -Message "Imported RDMI PowerShell modules successfully"
-
-    # Getting fqdn of rdsh vm
-    $SessionHostName = (Get-WmiObject win32_computersystem).DNSHostName + "." + (Get-WmiObject win32_computersystem).Domain
-    Write-Log  -Message "Getting fully qualified domain name of RDSH VM: $SessionHostName"
-
-    # Authenticating to WVD
-    if ($isServicePrincipal -eq "True")
-    {
-        Write-Log  -Message "Authenticating using service principal $TenantAdminCredentials.username and Tenant id: $AadTenantId "
-        $authentication = Add-RdsAccount -DeploymentUrl $RDBrokerURL -Credential $TenantAdminCredentials -ServicePrincipal -TenantId $AadTenantId 
-    }
-    else
-    {
-        Write-Log  -Message "Authenticating using user $($TenantAdminCredentials.username) "
-        $authentication = Add-RdsAccount -DeploymentUrl $RDBrokerURL -Credential $TenantAdminCredentials
-    }
-
-    Write-Log  -Message "Authentication object: $($authentication | Out-String)"
-    $obj = $authentication | Out-String
-
-    if ($authentication)
-    {
-        Write-Log -Message "RDMI Authentication successfully Done. Result:`n$obj"  
-    }
-    else
-    {
-        Write-Log -Error "RDMI Authentication Failed, Error:`n$obj"
-        throw "RDMI Authentication Failed, Error:`n$obj"
-    }
-
-    # Set context to the appropriate tenant group
-    Write-Log "Running switching to the $definedTenantGroupName context"
-    Set-RdsContext -TenantGroupName $definedTenantGroupName
-    try
-    {
-        $tenants = Get-RdsTenant -Name "$TenantName"
-        if (-Not $tenants)
-        {
-            Write-Log "No tenants exist or you do not have proper access."
-        }
-    }
-    catch
-    {
-        Write-Log -Message $_
-        throw $_
-    }
-
-    # Obtaining Registration Info
-    Start-Sleep (1..15 | Get-Random)
-    $Registered = New-RdsRegistrationInfo -TenantName "$TenantName" -HostPoolName "$HostPoolName" -ExpirationHours $Hours -ErrorAction SilentlyContinue
-    if (-Not $Registered)
-    {
-        $Registered = Export-RdsRegistrationInfo -TenantName "$TenantName" -HostPoolName "$HostPoolName" 
-        $obj =  $Registered | Out-String
-        Write-Log -Message "Exported Rds RegistrationInfo into variable 'Registered': $obj"
-    }
-    else
-    {
-        $obj =  $Registered | Out-String
-        Write-Log -Message "Created new Rds RegistrationInfo into variable 'Registered': $obj"
-    }
-
     # Executing DeployAgent psl file in rdsh vm and add to hostpool
     Write-Log "AgentInstaller is $DeployAgentLocation\RDAgentBootLoaderInstall, InfraInstaller is $DeployAgentLocation\RDInfraAgentInstall, SxS is $DeployAgentLocation\RDInfraSxSStackInstall"
     $DAgentInstall = .\DeployAgent.ps1 -AgentBootServiceInstallerFolder "$DeployAgentLocation\RDAgentBootLoaderInstall" `
@@ -152,15 +86,6 @@ else
                                        -rdshIs1809OrLater $rdshIs1809OrLaterBool
     
     Write-Log -Message "DeployAgent Script was successfully executed and RDAgentBootLoader,RDAgent,StackSxS installed inside VM for existing hostpool: $HostPoolName`n$DAgentInstall"
-
-    # Get Session Host Info
-    Write-Log -Message "Getting rdsh host $SessionHostName information"
-
-    [Microsoft.RDInfra.RDManagementData.RdMgmtSessionHost]$rdsh = ([PsRdsSessionHost]::new("$TenantName","$HostPoolName",$SessionHostName)).GetSessionHost()
-    Write-Log -Message "RDSH object content: `n$($rdsh | Out-String)"
-
-    $rdshName = $rdsh.SessionHostName | Out-String -Stream
-    $poolName = $rdsh.hostpoolname | Out-String -Stream
   
     Write-Log -Message "Successfully added $rdshName VM to $poolName"
 }
