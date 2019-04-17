c79e04299208e19723235057cb57bd24652f00ff
Susmita Pattanaik <susmita.pattanaik@ptgindia.com>
2019-04-17 18:57:07 +0530

    Aligned code in SessionHost api controller

--- wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring.Common/BAL/SessionHostBL.cs
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring.Common/BAL/SessionHostBL.cs
@@ -11,61 +11,32 @@ namespace MSFT.WVD.Monitoring.Common.BAL
 {
     public class SessionHostBL
     {
-        public IEnumerable<UserSession> GetUserSessions(string deploymentUrl, string accessToken, string tenantGroupName, string tenant, string hostPoolName, string sessionHostName)
+        public HttpResponseMessage GetUserSessions(string deploymentUrl, string accessToken, string tenantGroupName, string tenant, string hostPoolName, string sessionHostName)
         {
-            List<UserSession> userSessions;
             try
             {
-                HttpResponseMessage httpResponseMessage = CommonBL.InitializeHttpClient(deploymentUrl, accessToken).GetAsync($"/RdsManagement/V1/TenantGroups/{tenantGroupName}/Tenants/{tenant}/HostPools/{sessionHostName}/Sessions").Result;
-                string strJson = httpResponseMessage.Content.ReadAsStringAsync().Result;
-
-                if (httpResponseMessage.IsSuccessStatusCode)
-                {
-
-                    var arr = (JArray)JsonConvert.DeserializeObject(strJson);
-                    userSessions = ((JArray)arr).Select(item => new UserSession
-                    {
-                        tenantGroupName = (string)item["tenantGroupName"],
-                        tenantName = (string)item["tenantName"],
-                        hostPoolName = (string)item["hostPoolName"],
-                        sessionHostName = (string)item["sessionHostName"],
-                        userPrincipalName = (string)item["userPrincipalName"],
-                        sessionId = (int)item["sessionId"],
-                        applicationType = (string)item["applicationType"]
-                    }).ToList().Where(x => x.sessionHostName.ToString() == sessionHostName).ToList();
-                }
-                else
-                {
-                    var jobject = (JObject)JsonConvert.DeserializeObject(strJson);
-                    userSessions = new List<UserSession>
-                    {
-                       new UserSession()
-                       {
-                           ErrorDetails= new ErrorDetails()
-                           {
-                               StatusCode=(int)jobject["error"]["code"],
-                               Message= (string)jobject["error"]["message"]
-                           }
-                       }
-                   };
-                }
+                return CommonBL.InitializeHttpClient(deploymentUrl, accessToken).GetAsync($"/RdsManagement/V1/TenantGroups/{tenantGroupName}/Tenants/{tenant}/HostPools/{sessionHostName}/Sessions").Result;
             }
             catch (Exception ex)
             {
-                userSessions = new List<UserSession>
-                   {
-                       new UserSession
-                       {
-                           ErrorDetails= new ErrorDetails
-                           {
-                               StatusCode=(int)HttpStatusCode.InternalServerError,
-                               Message= ex.Message.ToString()
-                           }
-                       }
-                   };
+                return new HttpResponseMessage() { StatusCode = HttpStatusCode.BadRequest, Content = new StringContent(ex.Message) };
             }
+        }
 
-            return userSessions;
+        public IEnumerable<UserSession> GetUserSessions(HttpResponseMessage httpResponseMessage,string sessionHostName)
+        {
+            string strJson = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            var arr = (JArray)JsonConvert.DeserializeObject(strJson);
+            return  ((JArray)arr).Select(item => new UserSession
+            {
+                tenantGroupName = (string)item["tenantGroupName"],
+                tenantName = (string)item["tenantName"],
+                hostPoolName = (string)item["hostPoolName"],
+                sessionHostName = (string)item["sessionHostName"],
+                userPrincipalName = (string)item["userPrincipalName"],
+                sessionId = (int)item["sessionId"],
+                applicationType = (string)item["applicationType"]
+            }).ToList().Where(x => x.sessionHostName.ToString() == sessionHostName).ToList();
         }
 
         public HttpResponseMessage SendMessage(string deploymentUrl, string accessToken,SendMessageQuery sendMessageQuery)
--- wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/api/SessionHostController.cs
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring/api/SessionHostController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -26,32 +27,100 @@ namespace MSFT.WVD.Monitoring.api
         }
         // GET: api/<controller>
         [HttpGet("GetUserSessions")]
-        public IEnumerable<UserSession> GetUserSessions(string tenantGroupName,string tenant,string hostPoolName, string sessionHostName)
+        public IActionResult GetUserSessions(string tenantGroupName,string tenant,string hostPoolName, string sessionHostName)
         {
-            string token = Request.Headers["Authorization"];
-            _logger.LogInformation($"Call WVD api to get list of user sessions for selected tenant group {tenantGroupName} and tenant {tenant}");
+            try
+            {
+                string token = Request.Headers["Authorization"];
+                _logger.LogInformation($"Call WVD api to get list of user sessions for selected tenant group {tenantGroupName} and tenant {tenant}");
 
-            return sessionHostBL.GetUserSessions(Configuration.RDBrokerUrl, token, tenantGroupName, tenant, hostPoolName, sessionHostName);
+                HttpResponseMessage httpResponseMessage = sessionHostBL.GetUserSessions(Configuration.RDBrokerUrl, token, tenantGroupName, tenant, hostPoolName, sessionHostName);
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    IEnumerable<UserSession> connectionActivities = sessionHostBL.GetUserSessions(httpResponseMessage, sessionHostName);
+                    return new OkObjectResult(connectionActivities);
+                }
+                else
+                {
+                    var message = string.Empty;
+                    if (httpResponseMessage.Content != null)
+                    {
+                        message = !string.IsNullOrEmpty(httpResponseMessage.Content.ReadAsStringAsync().Result) ? httpResponseMessage.Content.ReadAsStringAsync().Result : httpResponseMessage.ReasonPhrase;
+                    }
+                    _logger.LogError($"Error on listing connected user : {message}");
+                    return StatusCode((int)httpResponseMessage.StatusCode, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.Message}");
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);
+            }
         }
 
        
         // POST api/<controller>
         [HttpPost("SendMessage")]
-        public HttpResponseMessage SendMessage(SendMessageQuery sendMessageQuery)
+        public IActionResult SendMessage(SendMessageQuery sendMessageQuery)
         {
-            string token = Request.Headers["Authorization"];
-            _logger.LogInformation($"Call WVD api to post message to connected user");
+            try
+            {
+                string token = Request.Headers["Authorization"];
+                _logger.LogInformation($"Call WVD api to post message to connected user");
 
-            return sessionHostBL.SendMessage(Configuration.RDBrokerUrl, token, sendMessageQuery);
+                HttpResponseMessage httpResponseMessage = sessionHostBL.SendMessage(Configuration.RDBrokerUrl, token, sendMessageQuery);
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    return new OkResult();
+                }
+                else
+                {
+                    var message = string.Empty;
+                    if (httpResponseMessage.Content != null)
+                    {
+                        message = !string.IsNullOrEmpty(httpResponseMessage.Content.ReadAsStringAsync().Result) ? httpResponseMessage.Content.ReadAsStringAsync().Result : httpResponseMessage.ReasonPhrase;
+                    }
+                    _logger.LogError($"Error on sending message to connected user{message}");
+                    return StatusCode((int)httpResponseMessage.StatusCode, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.Message}");
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);
+            }
         }
 
         [HttpPost("LogOffUser")]
-        public HttpResponseMessage LogOffUser(LogOffUserQuery logOffUserQuery)
+        public IActionResult LogOffUser(LogOffUserQuery logOffUserQuery)
         {
-            string token = Request.Headers["Authorization"];
-            _logger.LogInformation($"Call WVD api to logoff connected user");
+            try
+            {
+                string token = Request.Headers["Authorization"];
+                _logger.LogInformation($"Call WVD api to logoff connected user");
 
-            return sessionHostBL.LogOffUser(Configuration.RDBrokerUrl, token, logOffUserQuery);
+                HttpResponseMessage httpResponseMessage = sessionHostBL.LogOffUser(Configuration.RDBrokerUrl, token, logOffUserQuery);
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    return new OkResult();
+                }
+                else
+                {
+                    var message = string.Empty;
+                    if (httpResponseMessage.Content != null)
+                    {
+                        message = !string.IsNullOrEmpty(httpResponseMessage.Content.ReadAsStringAsync().Result) ? httpResponseMessage.Content.ReadAsStringAsync().Result : httpResponseMessage.ReasonPhrase;
+                    }
+                    _logger.LogError($"Error on logoff connected user : {message}");
+                    return StatusCode((int)httpResponseMessage.StatusCode, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.Message}");
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);
+            }
+           
         }
 
 
