1b0b22892c811244f90a254fbd7625b02c15984a
Susmita Pattanaik <susmita.pattanaik@ptgindia.com>
2019-06-06 10:52:50 +0530

    Removed unused config value from appsettings.json

--- wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring.Common/Services/CommonService.cs
+++ wvd-sh/wvd-monitoring-ux/src/MSFT.WVD.Monitoring.Common/Services/CommonService.cs
@@ -22,7 +22,7 @@ namespace MSFT.WVD.Monitoring.Common.Services
 
         public string GetAccessTokenLogAnalytic(string refreshToken)
         {
-
+            _logger.LogInformation("Get Access token using refresh token for log analytics api. ");
             HttpResponseMessage response;
             Dictionary<string, string> requestdata = new Dictionary<string, string>();
             var url = _config["configurations:AAD_Token_URL"];
@@ -42,14 +42,13 @@ namespace MSFT.WVD.Monitoring.Common.Services
 
         public string GetAccessTokenWVD(string refreshToken)
         {
-
+            _logger.LogInformation("Get Access token using refresh token for WVD API. ");
             HttpResponseMessage response;
             Dictionary<string, string> requestdata = new Dictionary<string, string>();
             var url = _config["configurations:AAD_Token_URL"];
             requestdata.Add("grant_type", "refresh_token");
             requestdata.Add("auth_url", _config["configurations:AAD_AUTH_URL"]);
             requestdata.Add("resource", _config["configurations:RESOURCE_URL"]);
-            requestdata.Add("redirect_uri", _config["configurations:REDIRECT_URL"]);
             requestdata.Add("client_id", _config["AzureAd:ClientId"]);
             requestdata.Add("refresh_token", refreshToken);
             using (HttpClient client = new HttpClient())
