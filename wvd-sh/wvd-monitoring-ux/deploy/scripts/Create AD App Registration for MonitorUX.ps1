57bb2c785abe48c8f057751c04f9fabd5c5432de
PeopleTech(CSE) <>
2019-06-21 18:48:56 +0530

    Added PowerShell scripts and updated azuredeploy.json file

---
+++ wvd-sh/wvd-monitoring-ux/deploy/scripts/Create AD App Registration for MonitorUX.ps1
@@ -0,0 +1,102 @@
+﻿<#
+
+.SYNOPSIS
+Create an Azure AD App Registration
+
+.DESCRIPTION
+This script is used to create an Azure AD App Registration
+
+.ROLE
+Users
+
+#>
+Param(
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $AADAppDisplayName
+
+)
+
+# Set the ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
+
+# Importing the modules
+Import-Module Azure
+Import-Module AzureAD
+
+# Initialize
+$ErrorActionPreference = "Stop"
+$spnRole = "contributor"
+
+# Provide the credentials to authenticate to Azure/AzureAD
+$Credentials=Get-Credential
+
+#Authenticating to Azure
+Login-AzureRmAccount -Credential $Credentials
+
+# Authentcating to AzureAD
+Connect-AzureAD -Credential $Credentials
+
+# Check if AD Application exist
+$existingApplication = Get-AzureRmADApplication -DisplayName $AADAppDisplayName -ErrorAction SilentlyContinue
+if ($existingApplication -ne $null) {
+    $appId = $existingApplication.ApplicationId
+    Write-Output "An AAD Application already exists with (Application Id: $appId). Choose a different app display name"  -Verbose
+    return
+}
+# Create an application secret
+$startDate = Get-Date
+$endDate = $startDate.AddYears($script:yearsOfExpiration)
+$Guid = New-Guid
+$PasswordCredential = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordCredential
+$PasswordCredential.StartDate = $startDate
+$PasswordCredential.EndDate = $startDate.AddYears(1)
+$PasswordCredential.KeyId = $Guid
+$PasswordCredential.Value = ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($Guid))))+"="
+$ClientSecret=$PasswordCredential.Value
+
+Write-Output "Creating a new Application in AAD" -Verbose
+
+# Create an unique App Registration Name
+$date=Get-Date -UFormat %d%m%y
+$DisplayName=$AADAppDisplayName+"-"+($date)
+
+# Create a new AD Application
+$azureAdApplication=New-AzureADApplication -DisplayName $DisplayName -AvailableToOtherTenants $false -Verbose -ErrorAction Stop
+$ClientId = $azureAdApplication.AppId
+Write-Output "Azure AAD Application creation completed successfully (Application Id: $ClientId)" -Verbose
+
+# Create new SPN
+Write-Output "Creating a new SPN" -Verbose
+$spn = New-AzureRmADServicePrincipal -ApplicationId $ClientId
+$spnName = $spn.ServicePrincipalNames
+Write-Output "SPN creation completed successfully (SPN Name: $spnName)" -Verbose
+
+# Assign role to SPN
+Write-Output "Waiting for SPN creation to reflect in Directory before Role assignment"
+Start-Sleep 20
+Write-Output "Assigning role ($spnRole) to SPN App ($ClientId)" -Verbose
+New-AzureRmRoleAssignment -RoleDefinitionName $spnRole -ServicePrincipalName $ClientId
+Write-Output "SPN role assignment completed successfully" -Verbose
+
+# Set windows virtual desktop permission to Client App Registration
+$WVDApiPrincipal = Get-AzureADServicePrincipal -SearchString "Windows Virtual Desktop" | Where-Object {$_.DisplayName -eq "Windows Virtual Desktop"}
+$AzureWVDApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+$AzureWVDApiAccess.ResourceAppId = $WVDApiPrincipal.AppId
+foreach($permission in $WVDApiPrincipal.Oauth2Permissions){
+    $AzureWVDApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+}
+# Set windows virtual desktop permission to Client App Registration
+$AzureLogAnalyticsApiPrincipal = Get-AzureADServicePrincipal -SearchString "Log Analytics API"
+$AzureLogAnalyticsApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+$AzureLogAnalyticsApiAccess.ResourceAppId = $AzureLogAnalyticsApiPrincipal.AppId
+foreach($permission in $AzureLogAnalyticsApiPrincipal.Oauth2Permissions){
+    $AzureLogAnalyticsApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+}
+
+Set-AzureADApplication -ObjectId $azureAdApplication.ObjectId -RequiredResourceAccess $AzureLogAnalyticsApiAccess,$AzureWVDApiAccess -ErrorAction Stop
+
+#Get the Client Id and Client Secret
+Write-Output "Client Id : $ClientId"
+Write-Output "Client Secret Key: $ClientSecret"
---
+++ wvd-sh/wvd-monitoring-ux/deploy/scripts/Create LogAnalyticsWorkspace for MonitorUX.ps1
@@ -0,0 +1,119 @@
+﻿<#
+
+.SYNOPSIS
+Create Log Analytics Workspace
+
+.DESCRIPTION
+This script is used to create Log Analytics Workspace
+
+.ROLE
+Users
+
+#>
+Param(
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ResourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $LogAnalyticsWorkspaceName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Location,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SubscriptionId
+
+)
+# Import the AzureRm module
+Import-Module AzureRM
+
+# Provide the credentias to authenticate to Azure
+$Credential=Get-Credential
+
+# Authenticate to Azure
+Login-AzureRmAccount -Credential $Credential
+
+# Select the specified subscription
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+# Check the specified resourcegroup exist/not if not will create new resource group
+$CheckRG = Get-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -ErrorAction SilentlyContinue
+if (!$CheckRG) {
+    Write-Output "The specified resourcegroup does not exist, creating the resourcegroup $ResourceGroupName..."
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force
+}
+
+# Create new Log Analytics Workspace
+$CheckLAW = Get-AzureRmOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $LogAnalyticsWorkspaceName -ErrorAction SilentlyContinue
+if (!$CheckLAW) {
+    Write-Output "The workspace $LogAnalyticsWorkspaceName does not exist, Creating..."
+    New-AzureRmOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $LogAnalyticsWorkspaceName -Location $Location -Sku free 
+    Write-Output "Log Analytics Workspace created suceessfully"
+}
+
+Write-Output "Adding the Performance Counters to the Log Analytics Workspace"
+
+# Adding the Logical Disk(% Free Space) Performance counter
+New-AzureRmOperationalInsightsWindowsPerformanceCounterDataSource `
+                         -ResourceGroupName $ResourceGroupName `
+                         -WorkspaceName $LogAnalyticsWorkspaceName `
+                         -ObjectName "LogicalDisk" `
+                         -InstanceName "*" `
+                         -CounterName "% Free Space" `
+                         -IntervalSeconds 60 `
+                         -Name "Windows Performance Counter"
+
+# Adding the Logical Disk(Avg. Disk Queue Length)Performance counter
+New-AzureRmOperationalInsightsWindowsPerformanceCounterDataSource `
+                         -ResourceGroupName $ResourceGroupName `
+                         -WorkspaceName $LogAnalyticsWorkspaceName `
+                         -ObjectName "LogicalDisk" `
+                         -InstanceName "C:" `
+                         -CounterName "Avg. Disk Queue Length" `
+                         -IntervalSeconds 60 `
+                         -Name "Windows Performance Counter1"
+
+# Adding the Memory(Available MBytes)Performance counter
+New-AzureRmOperationalInsightsWindowsPerformanceCounterDataSource `
+                         -ResourceGroupName $ResourceGroupName `
+                         -WorkspaceName $LogAnalyticsWorkspaceName `
+                         -ObjectName "Memory" `
+                         -InstanceName "*" `
+                         -CounterName "Available MBytes" `
+                         -IntervalSeconds 60 `
+                         -Name "Windows Performance Counter2"
+
+# Adding the Processor Information(% Processor Time)Performance counter
+New-AzureRmOperationalInsightsWindowsPerformanceCounterDataSource `
+                         -ResourceGroupName $ResourceGroupName `
+                         -WorkspaceName $LogAnalyticsWorkspaceName `
+                         -ObjectName "Processor Information" `
+                         -InstanceName "*" `
+                         -CounterName "% Processor Time" `
+                         -IntervalSeconds 60 `
+                         -Name "Windows Performance Counter3"
+
+# Adding the User Input Delay per Session(Max Input Delay)Performance counter
+New-AzureRmOperationalInsightsWindowsPerformanceCounterDataSource `
+                         -ResourceGroupName $ResourceGroupName `
+                         -WorkspaceName $LogAnalyticsWorkspaceName `
+                         -ObjectName "User Input Delay per Session" `
+                         -InstanceName "*" `
+                         -CounterName "Max Input Delay" `
+                         -IntervalSeconds 60 `
+                         -Name "Windows Performance Counter4"
+
+Write-Output "Performance Counters are successfully added to the workspace"
+
+# Get the Log Analytics Workspace
+$Workspace=Get-AzureRmOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $LogAnalyticsWorkspaceName
+
+# Get the log analytics workspace id
+$WorkspaceId=$workspace.CustomerId
+
+Write-Output "The Log Analytics Workspace Id: $WorkspaceId"
