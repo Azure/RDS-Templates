a35b13a423d743d7895e030fe5a2b4c1b739c3f2
peopletech-cse <ptgadmin-cse@peopletech.com>
2019-07-09 12:01:33 +0530

    modifed monitoring name to diagnostics

---
+++ wvd-templates/diagnostics-sample/src/MSFT.WVD.Diagnostics/Models/HomePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MSFT.WVD.Diagnostics.Common.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSFT.WVD.Diagnostics.Models
+{
+    public class HomePageViewModel :IPageViewModel
+    {
+        public RoleAssignment SelectedRole { get; set; }
+        public string SelectedTenantGroupName { get; set; }
+        public bool ShowDialog { get; set; }
+        public HomePageSubmitModel SubmitData { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class HomePageSubmitModel
+    {
+        [Required]
+        public string TenantGroupName { get; set; }
+        [Required]
+        public string TenantName { get; set; }
+    }
+}
