ab39c3becc62cf36699b70b971adc8626684952b
Marcelo Bastos <mbastos@microsoft.com>
2019-07-16 16:58:27 -0700

    Initial commit

---
+++ wvd-sh/joinhostpool/README.md
@@ -0,0 +1,13 @@
+# Create WVD-Diagnostics-UX Environment
+
+Deploy the web app to your azure environment.
+
+Click the button below to deploy:
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2FRDS-Templates%2Fwvd-sh\joinhostpool%2Fwvd-sh%2Fjoinhostpool%2Frdagentarm.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+## Azure resouces deployed
+- Azure App Service (Web App)
+- Azure App Service Service Plan (S1- Standard)
---
+++ wvd-sh/joinhostpool/RdAgentInstall.ps1
@@ -0,0 +1,21 @@
+param(
+      [string]$registrationToken,
+)
+
+#Extract MSIs
+Expand-Archive -path Agents.msi 
+$msiFile =  Get-ChildItem .\Agents -name 'Microsoft.RDInfra.RDAgent*'
+
+"Installing RDAgent..."
+msiexec /i ".\Agent\$msiFile" /passive REGISTRATIONTOKEN=$registrationToken
+"Verifiying RDAgent registry keys"
+if ((Test-Path -Path "HKLM:\SOFTWARE\Microsoft\RDInfraAgent") -eq $false) {(Start-Sleep -s 60)} ELSE {"RDinfraAgent Registry entry found"}
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RDInfraAgent"
+
+"Installing BootLoader..."
+msiexec /i ".\Atent\Microsoft.RDInfra.RDAgentBootLoader.Installer-x64.msi" /passive
+if ((Test-Path -Path "HKLM:\SOFTWARE\Microsoft\RDAgentBootLoader") -eq $false) {(Start-Sleep -s 60)} ELSE {"RDAgentBootLoader Registry entry found"}
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RDAgentBootLoader"
+"Installation completed"
+
+
