219f7bd931c15f61d578a83daf4aab2d5a77586a
Marcelo Bastos <mbastos@microsoft.com>
2019-07-18 13:07:26 -0700

    test

--- wvd-sh/joinhostpool/RdAgentInstall.ps1
+++ wvd-sh/joinhostpool/RdAgentInstall.ps1
@@ -6,16 +6,17 @@ param(
 Expand-Archive -path Agents.msi 
 $msiFile =  Get-ChildItem .\Agents -name 'Microsoft.RDInfra.RDAgent*'
 
-"Installing RDAgent..."
+write-host "Installing RDAgent..."
 msiexec /i ".\Agent\$msiFile" /passive REGISTRATIONTOKEN=$registrationToken
 "Verifiying RDAgent registry keys"
 if ((Test-Path -Path "HKLM:\SOFTWARE\Microsoft\RDInfraAgent") -eq $false) {(Start-Sleep -s 60)} ELSE {"RDinfraAgent Registry entry found"}
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RDInfraAgent"
 
-"Installing BootLoader..."
+write-host "Installing BootLoader..."
 msiexec /i ".\Atent\Microsoft.RDInfra.RDAgentBootLoader.Installer-x64.msi" /passive
 if ((Test-Path -Path "HKLM:\SOFTWARE\Microsoft\RDAgentBootLoader") -eq $false) {(Start-Sleep -s 60)} ELSE {"RDAgentBootLoader Registry entry found"}
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\RDAgentBootLoader"
-"Installation completed"
+
+write-host "Installation completed"
 
 
