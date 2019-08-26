31d7605e2e842018d99c5dc22579547e9a507707
PeopleTech(CSE) <>
2019-08-26 15:26:12 +0530

    Updated the script with the alignments

--- wvd-templates/diagnostics-sample/deploy/scripts/Create AD App Registration for Diagnostics.ps1
+++ wvd-templates/diagnostics-sample/deploy/scripts/Create AD App Registration for Diagnostics.ps1
@@ -15,7 +15,11 @@ Param(
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [string] $AppName
+    [string] $AppName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SubscriptionId
 
 )
 
@@ -26,90 +30,113 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Co
 Import-Module Az
 Import-Module AzureAD
 
-# Provide the credentials to authenticate to Azure and AzureAD
-$Credentials = Get-Credential
-
-# Authenticating to Azure
-Login-AzAccount -Credential $Credentials
-
-# Authenticating to AzureAD
-Connect-AzureAD -Credential $Credentials
-
-# Check whether the AD Application exist/ not
-$existingApplication = Get-AzADApplication -DisplayName $AppName -ErrorAction SilentlyContinue
-if ($existingApplication -ne $null) {
-    $appId = $existingApplication.ApplicationId
-    Write-Output "An AAD Application already exists with AppName $AppName(Application Id: $appId). Choose a different AppName"  -Verbose
-    return
+# Get the context
+$context= Get-AzContext
+if($context -eq $null)
+{
+  Write-Error "Please authenticate to Azure & Azure AD using Login-AzAccount and Connect-AzureAD cmdlets and then run this script"
+  exit
 }
 
-# Create a Client Secret
-$startDate = Get-Date
-$endDate = $startDate.AddYears(1)
-$Guid = New-Guid
-$PasswordCredential = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordCredential
-$PasswordCredential.StartDate = $startDate
-$PasswordCredential.EndDate = $startDate.AddYears(1)
-$PasswordCredential.KeyId = $Guid
-$PasswordCredential.Value = ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($Guid))))+"="
-$ClientSecret=$PasswordCredential.Value
+# Select the subscription
+Select-AzSubscription -SubscriptionId $SubscriptionId
 
-Write-Output "Creating a new Application in AAD" -Verbose
+# Get the Role Assignment of the authenticated user
+$RoleAssignment=Get-AzRoleAssignment -SignInName $context.Account
 
-# Create a new AD Application with provided AppName
-$azAdApplication=New-AzureADApplication -DisplayName $AppName -PublicClient $false -AvailableToOtherTenants $false
+# Validate whether the authenticated user having the Owner or Contributor role
+if($RoleAssignment.RoleDefinitionName -eq "Owner" -or $RoleAssignment.RoleDefinitionName -eq "Contributor")
+{
+    # Check whether the AD Application exist/ not
+    $existingApplication = Get-AzADApplication -DisplayName $AppName -ErrorAction SilentlyContinue
+    if ($existingApplication -ne $null) 
+    {
+        $appId = $existingApplication.ApplicationId
+        Write-Output "An AAD Application already exists with AppName $AppName(Application Id: $appId). Choose a different AppName"  -Verbose
+        exit
+    }
 
-# Create an app credential to the Application
-$SecureClientSecret=ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
-New-AzADAppCredential -ObjectId $azAdApplication.ObjectId -Password $SecureClientSecret -StartDate $startDate -EndDate $startDate.AddYears(1)
+    Try
+    {
+        # Create a new AD Application with provided AppName
+        $azAdApplication=New-AzureADApplication -DisplayName $AppName -PublicClient $false -AvailableToOtherTenants $false 
+    }
+    catch
+    {
+        Write-Error "You must call the Connect-AzureAD cmdlet before calling any other cmdlets"
+        exit
+    }
 
-# Get the ClientId
-$ClientId = $azAdApplication.AppId
+    # Create a Client Secret
+    $startDate = Get-Date
+    $endDate = $startDate.AddYears(1)
+    $Guid = New-Guid
+    $PasswordCredential = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordCredential
+    $PasswordCredential.StartDate = $startDate
+    $PasswordCredential.EndDate = $startDate.AddYears(1)
+    $PasswordCredential.KeyId = $Guid
+    $PasswordCredential.Value = ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(($Guid))))+"="
+    $ClientSecret=$PasswordCredential.Value
 
-Write-Output "Azure AAD Application creation completed successfully with AppName $AppName (Application Id is: $ClientId)" -Verbose
+    Write-Output "Creating a new Application in AAD" -Verbose
 
-# Create new Service Principal
-Write-Output "Creating a new Service Principal" -Verbose
-$ServicePrincipal = New-AzADServicePrincipal -ApplicationId $ClientId 
+    # Create an app credential to the Application
+    $SecureClientSecret=ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
+    New-AzADAppCredential -ObjectId $azAdApplication.ObjectId -Password $SecureClientSecret -StartDate $startDate -EndDate $startDate.AddYears(1)
 
-# Get the Service Principal
-Get-AzADServicePrincipal -ApplicationId $ClientId
-$ServicePrincipalName = $ServicePrincipal.ServicePrincipalNames
-Write-Output "Service Principal creation completed successfully with $ServicePrincipalName)" -Verbose
+    # Get the ClientId
+    $ClientId = $azAdApplication.AppId
+    Write-Output "Azure AAD Application creation completed successfully with AppName $AppName (Application Id is: $ClientId)" -Verbose
 
-# Assign role to Service Principal
-Write-Output "Waiting for SPN creation to reflect in Directory before Role assignment"
-Start-Sleep 25
-Write-Output "Assigning contributor role to Service Principal App ($ClientId)" -Verbose
-New-AzRoleAssignment -ApplicationId $ClientId -RoleDefinitionName "contributor"
-Write-Output "Service Principal role assignment completed successfully" -Verbose 
+    # Create new Service Principal
+    Write-Output "Creating a new Service Principal" -Verbose
+    $ServicePrincipal = New-AzADServicePrincipal -ApplicationId $ClientId 
 
-# Set windows virtual desktop permission to Client App Registration
-$WVDApiPrincipal = Get-AzureADServicePrincipal -SearchString "Windows Virtual Desktop" | Where-Object {$_.DisplayName -eq "Windows Virtual Desktop"}
-$AzureWVDApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-$AzureWVDApiAccess.ResourceAppId = $WVDApiPrincipal.AppId
-foreach($permission in $WVDApiPrincipal.Oauth2Permissions){
-    $AzureWVDApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+    # Get the Service Principal
+    Get-AzADServicePrincipal -ApplicationId $ClientId
+    $ServicePrincipalName = $ServicePrincipal.ServicePrincipalNames
+    Write-Output "Service Principal creation completed successfully with $ServicePrincipalName)" -Verbose
+
+    # Assign role to Service Principal
+    Write-Output "Waiting for Service Principal creation to reflect in Directory before Role assignment"
+    Start-Sleep 25
+    Write-Output "Assigning contributor role to Service Principal App ($ClientId)" -Verbose
+    New-AzRoleAssignment -ApplicationId $ClientId -RoleDefinitionName "contributor"
+    Write-Output "Service Principal role assignment completed successfully" -Verbose 
+
+    # Set windows virtual desktop permission to Client App Registration
+    $WVDApiPrincipal = Get-AzureADServicePrincipal -SearchString "Windows Virtual Desktop" | Where-Object {$_.DisplayName -eq "Windows Virtual Desktop"}
+    $AzureWVDApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+    $AzureWVDApiAccess.ResourceAppId = $WVDApiPrincipal.AppId
+    foreach($permission in $WVDApiPrincipal.Oauth2Permissions)
+    {
+        $AzureWVDApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+    }
+    # Set windows virtual desktop permission to Client App Registration
+    $AzureLogAnalyticsApiPrincipal = Get-AzureADServicePrincipal -SearchString "Log Analytics API"
+    $AzureLogAnalyticsApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+    $AzureLogAnalyticsApiAccess.ResourceAppId = $AzureLogAnalyticsApiPrincipal.AppId
+    foreach($permission in $AzureLogAnalyticsApiPrincipal.Oauth2Permissions)
+    {
+        $AzureLogAnalyticsApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+    }
+
+    # Set Microsoft Graph API permission to Client App Registration
+    $AzureGraphApiPrincipal = Get-AzureADServicePrincipal -SearchString "Microsoft Graph"
+    $AzureGraphApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+    $AzureGraphApiAccess.ResourceAppId = $AzureGraphApiPrincipal.AppId
+    $permission = $AzureGraphApiPrincipal.Oauth2Permissions | Where-Object {$_.Value -eq "User.Read"}
+    $AzureGraphApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+
+    # Add the WVD API,Log Analytics API and Microsoft Graph API permissions to the ADApplication
+    Set-AzureADApplication -ObjectId $azAdApplication.ObjectId -RequiredResourceAccess $AzureLogAnalyticsApiAccess,$AzureWVDApiAccess,$AzureGraphApiAccess -ErrorAction Stop
+
+    # Get the Client Id/Application Id and Client Secret
+    Write-Output "Client Id : $ClientId"
+    Write-Output "Client Secret Key: $ClientSecret"
 }
-# Set windows virtual desktop permission to Client App Registration
-$AzureLogAnalyticsApiPrincipal = Get-AzureADServicePrincipal -SearchString "Log Analytics API"
-$AzureLogAnalyticsApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-$AzureLogAnalyticsApiAccess.ResourceAppId = $AzureLogAnalyticsApiPrincipal.AppId
-foreach($permission in $AzureLogAnalyticsApiPrincipal.Oauth2Permissions){
-    $AzureLogAnalyticsApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
+else
+{
+   Write-Output "Authenticated user should have the Owner/Contributor permissions"
 }
 
-# Set Microsoft Graph API permission to Client App Registration
-$AzureGraphApiPrincipal = Get-AzureADServicePrincipal -SearchString "Microsoft Graph"
-$AzureGraphApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
-$AzureGraphApiAccess.ResourceAppId = $AzureGraphApiPrincipal.AppId
-$permission = $AzureGraphApiPrincipal.Oauth2Permissions | Where-Object {$_.Value -eq "User.Read"}
-$AzureGraphApiAccess.ResourceAccess += New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList $permission.Id,"Scope"
-
-
-# Add the WVD API,Log Analytics API and Microsoft Graph API permissions to the ADApplication
-Set-AzureADApplication -ObjectId $azAdApplication.ObjectId -RequiredResourceAccess $AzureLogAnalyticsApiAccess,$AzureWVDApiAccess,$AzureGraphApiAccess -ErrorAction Stop
-
-# Get the Client Id/Application Id and Client Secret
-Write-Output "Client Id : $ClientId"
-Write-Output "Client Secret Key: $ClientSecret"
