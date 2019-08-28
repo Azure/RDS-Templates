dc4dbc618c8fa591ee226781541e7f3a95610eba
People Tech (CSE) <admin-cse@peopletech.com>
2019-08-28 16:24:24 +0530

    Updated script files with few alignments

--- wvd-templates/diagnostics-sample/deploy/scripts/Update AD App Authentication for Diagnostics.ps1
+++ wvd-templates/diagnostics-sample/deploy/scripts/Update AD App Authentication for Diagnostics.ps1
@@ -34,18 +34,19 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Co
 Import-Module Az
 Import-Module AzureAD
 
-# Provide the Azure credentials to authenticate to Azure and AzureAD
-$Credentials=Get-Credential
-
-#Authenticating to Azure
-Login-AzAccount -Credential $Credentials
-
-# Authentcating to AzureAD
-Connect-AzureAD -Credential $Credentials
+# Get the context
+$context= Get-AzContext
+if($context -eq $null)
+{
+  Write-Error "Please authenticate to Azure & Azure AD using Login-AzAccount and Connect-AzureAD cmdlets and then run this script"
+  exit
+}
 
 # Select the specified subscription
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
+if($RoleAssignment.RoleDefinitionName -eq "Owner" -or $RoleAssignment.RoleDefinitionName -eq "Contributor")
+{
 # Get the web app URLs list
 $Hostnames=(Get-AzWebApp).DefaultHostName
 $URL = $RedirectURI.Trim("https://")
@@ -55,8 +56,16 @@ if($Hostnames -match $URL)
 {
 $ReplyUrl = "$RedirectURI/security/signin-callback"
 
+#Check if the user is authenticated AzureAD
+$ListAllADApps = Get-AzADApplication
+if($ListAllADApps -eq $null){
+		Write-Error "You must call the Connect-AzureAD cmdlet before calling any other cmdlets"
+        exit
+}
+
 # Get Azure AD App
 $AADApp = Get-AzADApplication -ApplicationId $ClientId
+
 
 $ReplyUrls = $AADApp.ReplyUrls
 
@@ -73,4 +82,9 @@ Write-Output "Redirect URI is successfully added to AAD Application Authenticati
 else
 {
 Write-Output "Please provide the valid RedirectURI"
+}
+}
+else
+{
+Write-Output "Authenticated user should have the Owner/Contributor permissions"
 }
