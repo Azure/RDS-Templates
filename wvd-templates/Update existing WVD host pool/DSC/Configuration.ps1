60f76bad4cfa780b73d66bb91dde598caf4efb53
Mohit Nakrani <monakran@microsoft.com>
2019-12-30 21:24:32 -0800

    separate DSC configuration for registering session host and cleaning up with last VM

--- wvd-templates/Update existing WVD host pool/DSC/Configuration.ps1
+++ wvd-templates/Update existing WVD host pool/DSC/Configuration.ps1
@@ -1,4 +1,80 @@
-configuration SessionHost
+
+function isRdshServer {
+    $rdshIsServer = $true
+
+    $OSVersionInfo = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    
+    if ($null -ne $OSVersionInfo) {
+        if ($null -ne $OSVersionInfo.InstallationType) {
+            $rdshIsServer = @{$true = $true; $false = $false}[$OSVersionInfo.InstallationType -eq "Server"]
+        }
+    }
+
+    return $rdshIsServer
+}
+configuration RegisterSessionHost
+{
+    param
+    (
+        [Parameter(mandatory = $true)]
+        [string]$RDBrokerURL,
+    
+        [Parameter(mandatory = $true)]
+        [string]$DefinedTenantGroupName,
+    
+        [Parameter(mandatory = $true)]
+        [string]$TenantName,
+    
+        [Parameter(mandatory = $true)]
+        [string]$HostPoolName,
+    
+        [Parameter(mandatory = $true)]
+        [string]$Hours,
+    
+        [Parameter(mandatory = $true)]
+        [PSCredential]$TenantAdminCredentials,
+		
+        [Parameter(mandatory = $false)]
+        [string]$isServicePrincipal = "False",
+    
+        [Parameter(Mandatory = $false)]
+        [AllowEmptyString()]
+        [string]$AadTenantId = ""
+    )
+
+    $ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
+    $rdshIsServer = isRdshServer
+
+    Node localhost
+    {
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+            ConfigurationMode  = "ApplyOnly"
+        }
+
+        if ($rdshIsServer) {
+            "$(get-date) - rdshIsServer = true: $rdshIsServer" | out-file c:\windows\temp\rdshIsServerResult.txt -Append
+            WindowsFeature RDS-RD-Server {
+                Ensure = "Present"
+                Name   = "RDS-RD-Server"
+            }
+        }
+
+        Script RegisterSessionHost {
+            GetScript  = {
+                return @{'Result' = ''}
+            }
+            SetScript  = {
+                & "$using:ScriptPath\Script-RdshServer.ps1" -RdBrokerURL $using:RDBrokerURL -DefinedTenantGroupName $using:DefinedTenantGroupName -TenantName $using:TenantName -HostPoolName $using:HostPoolName -Hours $using:Hours -TenantAdminCredentials $using:TenantAdminCredentials -isServicePrincipal $using:isServicePrincipal -aadTenantId $using:AadTenantId
+            }
+            TestScript = {
+                return (Test-path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent")
+            }
+        }
+    }
+}
+
+configuration RegisterSessionHostAndCleanup
 {
     param
     (
@@ -52,22 +128,11 @@ configuration SessionHost
         [int]$rdshNumberOfInstances,
 
         [Parameter(mandatory = $false)]
-        [string]$rdshPrefix,
-
-        # [Parameter(mandatory = $false)]
-        [switch]$Last
+        [string]$rdshPrefix
     )
 
-    $rdshIsServer = $true
     $ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
-
-    $OSVersionInfo = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-    
-    if ($OSVersionInfo -ne $null) {
-        if ($OSVersionInfo.InstallationType -ne $null) {
-            $rdshIsServer = @{$true = $true; $false = $false}[$OSVersionInfo.InstallationType -eq "Server"]
-        }
-    }
+    $rdshIsServer = isRdshServer
 
     Node localhost
     {
@@ -83,12 +148,13 @@ configuration SessionHost
                 Name   = "RDS-RD-Server"
             }
         }
-        Script ExecuteRdAgentInstallServer {
+
+        Script RegisterSessionHost {
             GetScript  = {
                 return @{'Result' = ''}
             }
             SetScript  = {
-                & "$using:ScriptPath\Script-RdshServer.ps1" -RdBrokerURL $using:RDBrokerURL -DefinedTenantGroupName $using:DefinedTenantGroupName -TenantName $using:TenantName -HostPoolName $using:HostPoolName -Hours $using:Hours -TenantAdminCredentials $using:TenantAdminCredentials -isServicePrincipal $using:isServicePrincipal -aadTenantId $using:AadTenantId
+                & "$using:ScriptPath\Script-RegisterSessionHost.ps1" -RdBrokerURL $using:RDBrokerURL -DefinedTenantGroupName $using:DefinedTenantGroupName -TenantName $using:TenantName -HostPoolName $using:HostPoolName -Hours $using:Hours -TenantAdminCredentials $using:TenantAdminCredentials -isServicePrincipal $using:isServicePrincipal -aadTenantId $using:AadTenantId
             }
             TestScript = {
                 return (Test-path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent")
@@ -103,7 +169,8 @@ configuration SessionHost
                 & "$using:ScriptPath\Script-CleanupOldRdshSessionHosts.ps1" -RdBrokerURL $using:RDBrokerURL -DefinedTenantGroupName $using:DefinedTenantGroupName -TenantName $using:TenantName -HostPoolName $using:HostPoolName -TenantAdminCredentials $using:TenantAdminCredentials -AdAdminCredentials $using:AdAdminCredentials -isServicePrincipal $using:isServicePrincipal -aadTenantId $using:AadTenantId -SubscriptionId $using:SubscriptionId -userLogoffDelayInMinutes $using:userLogoffDelayInMinutes -userNotificationMessege $using:userNotificationMessege -messageTitle $using:messageTitle -deleteordeallocateVMs $using:deleteordeallocateVMs -DomainName $using:DomainName -rdshNumberOfInstances $using:rdshNumberOfInstances -rdshPrefix $using:rdshPrefix
             }
             TestScript = {
-                return (!$using:Last)
+                #//todo test this
+                return $false
             }
         }
     }
