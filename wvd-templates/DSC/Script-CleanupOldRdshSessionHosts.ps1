04fa00e07f166e0606e784b2b0fc6637249b7f63
Marcel Soklaski <lintFan@users.noreply.github.com>
2020-01-31 16:44:45 -0800

    Centralize Set-RdsContext calls into SetTenantGroupContextAndValidate which has improved error info

--- wvd-templates/DSC/Script-CleanupOldRdshSessionHosts.ps1
+++ wvd-templates/DSC/Script-CleanupOldRdshSessionHosts.ps1
@@ -83,29 +83,14 @@ ImportRDPSMod -Source $RDPSModSource -ArtifactsPath $ScriptPath
 # Authenticating to Windows Virtual Desktop
 . AuthenticateRdsAccount -DeploymentUrl $RDBrokerURL -Credential $TenantAdminCredentials -ServicePrincipal:($isServicePrincipal -eq 'True') -TenantId $AadTenantId
 
-# Set context to the appropriate tenant group
-$currentTenantGroupName = (Get-RdsContext).TenantGroupName
-if ($definedTenantGroupName -ne $currentTenantGroupName) {
-    Write-Log -Message "Running switching to the $definedTenantGroupName context"
-    Set-RdsContext -TenantGroupName $definedTenantGroupName
-}
-try {
-    $tenants = Get-RdsTenant -Name "$TenantName"
-    if (!$tenants) {
-        Write-Log "No tenants exist or you do not have proper access."
-    }
-}
-catch {
-    Write-Log -Message $_
-    throw $_
-}
+SetTenantGroupContextAndValidate -TenantGroupName $definedTenantGroupName -TenantName $TenantName
 
 # Checking if host pool exists.
 Write-Log -Message "Checking Hostpool exists inside the Tenant"
 $HostPool = Get-RdsHostPool -TenantName "$TenantName" -Name "$HostPoolName" -ErrorAction SilentlyContinue
 if (!$HostPool) {
     Write-Log -Error "$HostpoolName Hostpool does not exist in $TenantName Tenant"
-    throw "$HostpoolName Hostpool does not exist in $TnenatName Tenant"
+    throw "$HostpoolName Hostpool does not exist in $TenantName Tenant"
 }
 
 Write-Log -Message "Hostpool exists inside tenant: $TenantName"
