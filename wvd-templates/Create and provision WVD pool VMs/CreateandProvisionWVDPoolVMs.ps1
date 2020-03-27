a8662ca8f3e038b911a3d9b803201b46b8b8795d
EvanBasalik <evanba@microsoft.com>
2020-03-27 17:44:50 -0400

    Reworked loop logic

--- wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
+++ wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
@@ -1,6 +1,8 @@
 [int]$desiredPoolVMCount=100
 [int]$allocationBatchSize=25
-[int]$targetMinimumConcurrentAllocationsPercentage=80
+[string]$batchNamingPrefix="WVDDeploymentBatch"
+[single]$minimumConcurrentAllocationsPercentage=0.8
+[int]$sleepTimeMin=1
 $sleepIntervalMin=5
 $resourceGroupName="WVDTestRG"
 $location="EastUS"
@@ -52,38 +54,82 @@ $countExistingVMs = $existingVMs.count
 #now, figure out how many more VMs need created
 $countAdditionalVMs = $desiredPoolVMCount - $countExistingVMs
 
-#generate logic flow is as follows:
+#general logic flow is as follows:
 #deploy up to the allocation batch size
 #sleep for a bit
 #wake up and check if we have less deployments running than specified percentage of the batch size 
 #if so, then kick off a deployment of VMs equal to the delta
 #if not, do nothing
 
+#deploy the first batch
+[int]$vmsToDeploy=$allocationBatchSize
+New-AzResourceGroupDeployment `
+-Name $($batchNamingPrefix)0 `
+-ResourceGroupName $resourceGroupName `
+-virtualMachineCount $vmsToDeploy `
+-TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-copy-managed-disks/azuredeploy.json" `
+#        -TemplateParameterFile "C:\Users\evanba\source\repos\RDS-Templates\wvd-templates\Create and provision WVD pool VMs\parameters.json" 
+
+
 #start looping through creating VMs
+[int]$deploymentIteration=0
 do {
 
-    #deploy either the total desired or the allocation pool count - whichever is smaller
-    [int]$vmsToDeploy
-    Switch ($allocationBatchSize > $countAdditionalVMs)
-    {
-        $false { $vmsToDeploy = $countAdditionalVMs }
-        $true { $vmsToDeploy = $allocationBatchSize }
-    }
+    #get the count of active deployments from the *previous* batch
+    $countofVMDeployments = get-azresourcegroupdeploymentoperation `
+        -DeploymentName $($batchNamingPrefix)$($deploymentIteration) -ResourceGroupName $resourceGroupName `
+        | Where-Object {$_.properties.targetResource -match "virtualMachines"} `
+        | select -ExpandProperty properties 
 
-    #kick off an ARM deployment to deploy the number of VMs just calculated
-    New-AzResourceGroupDeployment `
-        -Name "testdeploy" `
+    #get the count of the completed deployments from the *previous* batch
+    $countofVMDeploymentsCompleted = get-azresourcegroupdeploymentoperation -DeploymentName testdeploy10 -ResourceGroupName wvdrg3 `
+    | Where-Object {$_.properties.targetResource -match "virtualMachines"} `
+    | select -ExpandProperty properties `
+    | Where-Object {$_.provisioningState -match "Succeeded"}
+
+    #see if ratio is below target ratio
+    [bool]$doMoreDeployments = $false
+    [int]$vmsToDeployIncrement = 0
+    if (1-$countofVMDeploymentsCompleted/$countofVMDeployments -lt $minimumConcurrentAllocationsPercentage)
+    {
+        #deploy more VMs
+
+        #see how many VMs exist
+        $existingVMs = Get-AzVM -ResourceGroupName $resourceGroupName
+        $countExistingVMs = $existingVMs.count
+
+        #now, figure out how many more VMs need created
+        $countAdditionalVMs = $desiredPoolVMCount - $countExistingVMs
+
+        #deploy either the total desired or the allocation pool count - whichever is smaller
+        Switch ($allocationBatchSize > $countAdditionalVMs)
+        {
+            $true { $vmsToDeployIncrement = $countAdditionalVMs }
+            $false { $vmsToDeployIncrement = $allocationBatchSize * (1-$minimumConcurrentAllocationsPercentage)}
+        }
+
+        #kick off an ARM deployment to deploy the number of VMs just calculated
+        #because we are doing this via a template, just increment the total VM count
+        #it has the additional benefit of potentially fixing up any busted VMs
+        #because ARM templates are declarative
+        $vmsToDeploy += $vmsToDeployIncrement
+        New-AzResourceGroupDeployment `
+        -Name $($batchNamingPrefix)$($deploymentIteration) `
         -ResourceGroupName $resourceGroupName `
         -virtualMachineCount $vmsToDeploy `
         -TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/201-vm-copy-managed-disks/azuredeploy.json" `
-        -DeploymentDebugLogLevel ResponseContent
-#        -TemplateParameterFile "C:\Users\evanba\source\repos\RDS-Templates\wvd-templates\Create and provision WVD pool VMs\parameters.json" 
+    #        -TemplateParameterFile "C:\Users\evanba\source\repos\RDS-Templates\wvd-templates\Create and provision WVD pool VMs\parameters.json" 
+
+    }
+
+
+
 
     #sleep for a bit
 
     #wake up and check if we have less deployments running than specified percentage of the batch size 
 
-    #get the count of deployments
+    #get the count of active deployments from the *previous* batch
     $countofVMDeployments = get-azresourcegroupdeploymentoperation -DeploymentName testdeploy10 -ResourceGroupName wvdrg3 `
     | Where-Object {$_.properties.targetResource -match "virtualMachines"} `
     | select -ExpandProperty properties `
@@ -101,6 +147,9 @@ do {
 
     #update the count of how many more we need
     $countAdditionalVMs = $desiredPoolVMCount - $countExistingVMs
+
+    #increment the loop counter so the next iteration gets a different deployment name
+    $deploymentIteration += 1
     
     #if not, do nothing
 
