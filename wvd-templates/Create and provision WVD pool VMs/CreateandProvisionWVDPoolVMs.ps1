19043c6b986379e1c512bcc05745375d8c482483
EvanBasalik <evanba@microsoft.com>
2020-03-28 12:59:25 -0400

    Reference for random password

--- wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
+++ wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
@@ -11,6 +11,7 @@ $targetSubnetName="default"
 [int]$maxSimulanteousDeployments=3
 [array]$deployments = @()
 [string]$userName="user01"
+#create a random password that meet's Azure's rules - https://gallery.technet.microsoft.com/office/Generate-Random-Password-ca4c9f07
 [string]$password=(-join(65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90|%{[char]$_}|Get-Random -C 2)) + (-join(97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122|%{[char]$_}|Get-Random -C 2)) + (-join(65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90|%{[char]$_}|Get-Random -C 2)) + (-join(97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122|%{[char]$_}|Get-Random -C 2)) + (-join(64,33,35,36|%{[char]$_}|Get-Random -C 1))  + (-join(49,50,51,52,53,54,55,56,57|%{[char]$_}|Get-Random -C 3)) 
 
 #Connect-AzAccount
