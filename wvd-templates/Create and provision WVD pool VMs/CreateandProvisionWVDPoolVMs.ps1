88ba261add12129e32f499eaf33914ddffa09275
EvanBasalik <evanba@microsoft.com>
2020-03-30 22:14:07 -0400

    Fix bug counting existing VMs in resource group

--- wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
+++ wvd-templates/Create and provision WVD pool VMs/CreateandProvisionWVDPoolVMs.ps1
@@ -1,9 +1,14 @@
+#great tip from https://www.gngrninja.com/script-ninja/2016/2/12/powershell-quick-tip-simple-logging-with-timestamps
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
 $isTesting = $true
 [int]$desiredPoolVMCount=5
 [int]$allocationBatchSize=1
 [int]$maxSimulanteousDeployments = 3
 [array]$deployments = @()
-[int]$sleepTimeMin=1
+[int]$sleepTimeMin=5
 [string]$batchNamingPrefix="WVDDeploymentBatch"
 $resourceGroupName="WVDTestRG"
 $location="centralus"
@@ -12,14 +17,13 @@ $targetVNETName="fabrikam-central"
 $targetSubnetName="desktops"
 $virtualNetworkResourceGroupName = "fabrikamwvd-central"
 
-
 #enforce most current rules to help catch run-time failures
 Set-StrictMode -Version Latest
 
 #Connect-AzAccount
 #for testing
 if ($isTesting) {
-    Write-Debug "Running in Testing mode"
+    Write-Host "$(Get-TimeStamp) Running in Testing mode. Resource will have a GUID appended"
     $resourceGroupName += New-Guid
 }
 
@@ -52,23 +56,6 @@ if ($notPresent)
     $virtualNetwork | Set-AzVirtualNetwork
 }
 
-
-[int]$countExistingVMs=0
-[int]$countAdditionalVMs=0
-
-#since we know how many VMs we want, let's figure out how many we need to deploy
-#query to see how many VMs already exist
-$existingVMs = @()
-$existingVMs = Get-AzVM -ResourceGroupName $resourceGroupName
-if (!$existingVMs) {
-    #no VMs in the resource group yet
-    $countExistingVMs = 0
-}
-else {
-    #VMs already exist, so use the count
-    $countExistingVMs = $existingVMs.Count
-}
-
 #general logic flow is as follows:
 #deploy up to maxSimulanteousDeployments each one having the allocation batch size
 #sleep for a bit
@@ -80,20 +67,40 @@ else {
 [int]$deploymentIteration=0
 do {
 
+    #see how many VMs are in the resource group
     #loop through all the deployments which aren't already marked as completed
     #if they are done, update the Completed property
     #NOTE: for some reason Get-AzDeployment doesn't return any results (RBAC?) so am forced to use custom array
     #TODO: Switch this to checking on the job status
 
-    Write-Host "Waking up to try and run another deployment"
+    #since we know how many VMs we want, let's figure out how many we need to deploy
+    #query to see how many VMs already exist
+    [int]$countExistingVMs=0
+    [int]$countAdditionalVMs=0
+    $existingVMs = @()
+    $existingVMs = Get-AzVM -ResourceGroupName $resourceGroupName
+    if (!$existingVMs) {
+        #no VMs in the resource group yet
+        $countExistingVMs = 0
+    }
+    else {
+        #VMs already exist, so use the count
+        $countExistingVMs = ($existingVMs | Measure-Object).Count
+    }
+    Write-Host "$(Get-TimeStamp) VMs already in $($resourceGroupName): $($countExistingVMs)"
+
+    Write-Host "$(Get-TimeStamp) Waking up to try and run another deployment"
     foreach ($deployment in $deployments) {
         if ($deployment.Completed -eq $false) {
+
+            Write-Host "$(Get-TimeStamp) Checking status on $($deployment.Name)"
 
             #get all the operations running
             $runningOperations = @(get-azresourcegroupdeploymentoperation `
             -DeploymentName $deployment.Name -ResourceGroupName $resourceGroupName `
             | Select-Object -ExpandProperty properties)
 #            | Where-Object {(($_.provisioningState -match "Running") -or ($_.provisioningState -match "Succeeded"))}
+            Write-Host "$(Get-TimeStamp) Running operations: $($runningOperations.Count)"
 
             #if there are any, then see if any are VMs
             #there HAVE to be VM operations at some point so if none exist then we haven't gotten far enough
@@ -101,18 +108,24 @@ do {
                 
                 #filter down to just Create operations
                 $createOperations = @($runningOperations | Where-Object {$_.provisioningOperation -match "Create"})
+                Write-Host "$(Get-TimeStamp) Create operations: $($createOperations.Count)"
+
                 if (($createOperations | Measure-Object).Count -gt 0)
                 {
                     #filter down to just VMs
                     $vmOperations = @($createOperations | Where-Object {$_.targetResource -match "virtualMachines"})
+                    Write-Host "$(Get-TimeStamp) VM operations: $($vmOperations.Count)"
+
                     if (($vmOperations | Measure-Object).Count -gt 0) {
 
                         #find just the VM operations that are still running
                         $runningVMOperations = @($vmOperations | Where-Object {$_.provisioningState -match "Running"})
+                        Write-Host "$(Get-TimeStamp) Running VM operations: $($runningVMOperations.Count)"
 
                         #if none are still running, then we either fully completed or failed - either is acceptable here
                         if (($runningVMOperations | Measure-Object).Count -eq 0)
                         {
+                            Write-Host "$(Get-TimeStamp) Marking $($deployment.Name) as completed"
                             $deployment.Completed=$true
                         }
                     }
@@ -126,7 +139,7 @@ do {
     $needMoreDeployments = $false
     if (!$deployments) {
         #if no deployments at all, then allow more to kick off
-        Write-Host "No deployments initiated - allowing a new one to run"
+        Write-Host "$(Get-TimeStamp) No deployments initiated - allowing a new one to run"
         $needMoreDeployments = $true
     }
     else {
@@ -135,11 +148,11 @@ do {
 
             #less than $maxSimultaneousDeployments, so allow more to kick off
             $needMoreDeployments = $true
-            Write-Host "Less than $($maxSimulanteousDeployments) running - allow a new one to run"
+            Write-Host "$(Get-TimeStamp) Less than $($maxSimulanteousDeployments) running - allow a new one to run"
         }
         else
         {
-            Write-Host "More than $($maxSimulanteousDeployments) running - blocking new deployments"
+            Write-Host "$(Get-TimeStamp) More than $($maxSimulanteousDeployments) running - blocking new deployments"
             $needMoreDeployments = $false
         }
     }
@@ -149,28 +162,28 @@ do {
     if ($needMoreDeployments)
     {
         #now, figure out how many more VMs need created
-        Write-Host "Desired total VMs: $($desiredPoolVMCount)"
-        Write-Host "Existing VMs in pool: $($countExistingVMs)"
+        Write-Host "$(Get-TimeStamp) Desired total VMs: $($desiredPoolVMCount)"
+        Write-Host "$(Get-TimeStamp) Existing VMs in pool: $($countExistingVMs)"
 
         #assume that all previous deployments will complete successfully
         #so add that to the count of existing VMs
         #May not be 100% accurate, but should be close in general
         $deployingVMs = $deploymentIteration * $allocationBatchSize
-        Write-Host "VMs already deploying: $($deployingVMs)"
+        Write-Host "$(Get-TimeStamp) VMs already deploying: $($deployingVMs)"
 
         $countAdditionalVMs = $desiredPoolVMCount - $countExistingVMs - $deployingVMs
-        Write-Host "Additional VMs needed: $($countAdditionalVMs)"
+        Write-Host "$(Get-TimeStamp) Additional VMs needed: $($countAdditionalVMs)"
 
         #deploy either the total desired or the allocation pool count - whichever is smaller
         Switch ($allocationBatchSize -gt $countAdditionalVMs)
         {
             $true { 
                 $vmsToDeploy = $countAdditionalVMs 
-                Write-Debug "$($allocationBatchSize)>$($countAdditionalVMs) - Additional VMs smallest. Only deploying what is needed"
+                Write-Debug "$(Get-TimeStamp) $($allocationBatchSize)>$($countAdditionalVMs) - Additional VMs smallest. Only deploying what is needed"
             }
             $false {
                  $vmsToDeploy = $allocationBatchSize 
-                 Write-Debug "$($allocationBatchSize)<$($countAdditionalVMs) - Batch size smallest. Deploying full batch"
+                 Write-Debug "$(Get-TimeStamp) $($allocationBatchSize)<$($countAdditionalVMs) - Batch size smallest. Deploying full batch"
             }
         }
 
@@ -193,18 +206,18 @@ do {
         if ((Get-Job -Name "$($deploymentName)").State -ne "Failed") {
 
             #add the new deployment to the array for tracking purposes
-            Write-Host "Successfully started deployment $($deploymentName)"
+            Write-Host "$(Get-TimeStamp) Successfully started deployment $($deploymentName)"
             $deployment = New-Object -TypeName PSObject
             $deployment | Add-Member -Name 'Name' -MemberType Noteproperty -Value $deploymentName
             $deployment | Add-Member -Name 'Completed' -MemberType Noteproperty -Value $false
             $deployments += $deployment
-            Write-Debug "Added $($deploymentName) to tracking array"
+            Write-Debug "$(Get-TimeStamp) Added $($deploymentName) to tracking array"
     
             #increment the loop counter so the next iteration gets a different deployment name
             $deploymentIteration += 1
         }    
         else {
-            Write-Host "$($deploymentName) failed validation"
+            Write-Host "$(Get-TimeStamp) $($deploymentName) failed validation"
             #job is in a failed state - report the reason
             $job = Get-Job -Name "$($deploymentName)"
             throw Receive-Job -Job $job -Keep
@@ -213,7 +226,7 @@ do {
     }
 
     #sleep for a bit
-    Write-Host "Sleeping for $(60*$sleepTimeMin) seconds to let deployments run"
+    Write-Host "$(Get-TimeStamp) Sleeping for $(60*$sleepTimeMin) seconds to let deployments run"
     Start-Sleep -s (60*$sleepTimeMin)
 
 } while ($countAdditionalVMs -gt $allocationBatchSize)
