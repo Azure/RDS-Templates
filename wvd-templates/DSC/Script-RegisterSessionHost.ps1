d62e7b2610faaf997c529f269fae6a1973cd7ad1
Mohit Nakrani <monakran@microsoft.com>
2020-04-23 23:24:51 -0700

    remove object def from functions, refactor polling for session host avail & add support for the new desired states in register session host

--- wvd-templates/DSC/Script-RegisterSessionHost.ps1
+++ wvd-templates/DSC/Script-RegisterSessionHost.ps1
@@ -105,28 +105,39 @@ Write-Log -Message "DeployAgent Script was successfully executed and RDAgentBoot
 
 # Get Session Host Info
 Write-Log -Message "Getting RDSH session host info for '$SessionHostName'"
-[PsRdsSessionHost]$pssh = [PsRdsSessionHost]::new($TenantName, $HostPoolName, $SessionHostName)
 
-Write-Log -Message "Waiting for session host to be in available state"
-$AvailableSh = $pssh.GetSessionHostWhenAvailable()
-if ($null -ne $AvailableSh) {
-    Write-Log -Message "Session host $($SessionHostName) is now in Available state"
+# Wait for the session host to be available
+$StartTime = Get-Date
+$SessionHost = $null
+$TimeOutInSec = 900
+$DesiredStates = ('Available', 'NeedsAssistance')
+
+Write-Log -Message "Wait for sessions host to be in any of the desired states: $($DesiredStates -join ', ')"
+while ((!$SessionHost -or $SessionHost.Status -notin $DesiredStates) -and (get-date).Subtract($StartTime).TotalSeconds -lt $TimeOutInSec) {
+    write-log "Session host is in '$($SessionHost.Status)' state, continue waiting"
+    Start-Sleep -Seconds 30
+    $SessionHost = Get-RdsSessionHost -TenantName $TenantName -HostPoolName $HostPoolName -Name $SessionHostName -ErrorAction SilentlyContinue
+}
+
+Write-Log -Message "RDSH object content: `n$($SessionHost | Out-String)"
+if ($SessionHost -and $SessionHost.Status -in $DesiredStates) {
+    Write-Log -Message "Session host is now in '$($SessionHost.Status)' state"
 }
 else {
-    Write-Log -Err "Session host $($SessionHostName) not in Available state, wait timed out (threshold is $($pssh.TimeoutInSec) seconds)"
+    Write-Log -Err "Session host is not in any of the desired states: $($DesiredStates -join ', '), wait timed out (threshold is $TimeOutInSec seconds)"
 }
 
 # check if the session host was successfully registered to host pool, note that the error is thrown because the TestScript configuration of DSC may not be run after SetScript (this script)
 Write-Log -Message "Check RD Infra registry to see if RD Agent is registered"
 $RDInfraReg = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent" -ErrorAction SilentlyContinue
 if (!$RDInfraReg) {
-    throw "RD Agent failed to register '$SessionHostName' VM to '$HostPoolName' (RD Infra registry missing)"
+    throw "RD Agent failed to register VM '$SessionHostName' to HostPool '$HostPoolName' (RD Infra registry missing)"
 }
 if ($RDInfraReg.RegistrationToken -ne '') {
-    throw "RD Agent failed to register '$SessionHostName' VM to '$HostPoolName' (RegistrationToken in RD Infra registry is not empty: '$($RDInfraReg.RegistrationToken)')"
+    throw "RD Agent failed to register VM '$SessionHostName' to HostPool '$HostPoolName' (RegistrationToken in RD Infra registry is not empty: '$($RDInfraReg.RegistrationToken)')"
 }
 if ($RDInfraReg.IsRegistered -ne 1) {
-    throw "RD Agent failed to register '$SessionHostName' VM to '$HostPoolName' (Value of 'IsRegistered' in RD Infra registry is not 1: $($RDInfraReg.IsRegistered))"
+    throw "RD Agent failed to register VM '$SessionHostName' to HostPool '$HostPoolName' (Value of 'IsRegistered' in RD Infra registry is not 1: $($RDInfraReg.IsRegistered))"
 }
 
-Write-Log -Message "Successfully registered '$SessionHostName' VM to '$HostPoolName'"
+Write-Log -Message "Successfully registered VM '$SessionHostName' to HostPool '$HostPoolName'"
