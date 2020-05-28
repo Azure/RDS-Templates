a519ae96e2ec6963fa2b08e76cfd17530244c630
David Tran <datr@microsoft.com>
2020-05-28 14:21:15 -0700

    add spring templates

---
+++ spring20 wvd-templates/DSC/Script-AddRdshServer.ps1
@@ -0,0 +1,64 @@
+﻿<#
+
+.SYNOPSIS
+Creating Hostpool and add sessionhost servers to existing/new Hostpool.
+
+.DESCRIPTION
+This script add sessionhost servers to existing/new Hostpool
+The supported Operating Systems Windows Server 2016.
+
+.ROLE
+Readers
+
+#>
+param(
+    [Parameter(mandatory = $true)]
+    [string]$HostPoolName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$RegistrationInfoToken
+)
+
+$ScriptPath = [system.io.path]::GetDirectoryName($PSCommandPath)
+
+# Dot sourcing Functions.ps1 file
+. (Join-Path $ScriptPath "Functions.ps1")
+
+# Setting ErrorActionPreference to stop script execution when error occurs
+$ErrorActionPreference = "Stop"
+
+Write-Log -Message "Identifying if this VM is Build >= 1809"
+$rdshIs1809OrLaterBool = Is1809OrLater
+
+Write-Log -Message "Creating a folder inside rdsh vm for extracting deployagent zip file"
+$DeployAgentLocation = "C:\DeployAgent"
+ExtractDeploymentAgentZipFile -ScriptPath $ScriptPath -DeployAgentLocation $DeployAgentLocation
+
+Write-Log -Message "Changing current folder to Deployagent folder: $DeployAgentLocation"
+Set-Location "$DeployAgentLocation"
+
+# Checking if RDInfragent is registered or not in rdsh vm
+$CheckRegistry = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent" -ErrorAction SilentlyContinue
+
+Write-Log -Message "Checking whether VM was Registered with RDInfraAgent"
+
+if ($CheckRegistry)
+{
+    Write-Log -Message "VM was already registered with RDInfraAgent, script execution was stopped"
+}
+else
+{
+    Write-Log -Message "VM not registered with RDInfraAgent, script execution will continue"
+
+    # Executing DeployAgent psl file in rdsh vm and add to hostpool
+    Write-Log "AgentInstaller is $DeployAgentLocation\RDAgentBootLoaderInstall, InfraInstaller is $DeployAgentLocation\RDInfraAgentInstall, SxS is $DeployAgentLocation\RDInfraSxSStackInstall"
+    $DAgentInstall = .\DeployAgent.ps1 -AgentBootServiceInstallerFolder "$DeployAgentLocation\RDAgentBootLoaderInstall" `
+                                       -AgentInstallerFolder "$DeployAgentLocation\RDInfraAgentInstall" `
+                                       -SxSStackInstallerFolder "$DeployAgentLocation\RDInfraSxSStackInstall" `
+                                       -EnableSxSStackScriptFolder "$DeployAgentLocation\EnableSxSStackScript" `
+                                       -RegistrationToken $RegistrationInfoToken `
+                                       -StartAgent $true `
+                                       -rdshIs1809OrLater $rdshIs1809OrLaterBool
+    
+    Write-Log -Message "DeployAgent Script was successfully executed and RDAgentBootLoader,RDAgent,StackSxS installed inside VM for existing hostpool: $HostPoolName`n$DAgentInstall"
+}
