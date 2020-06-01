4deaede36a499047df5dc1ca2fd4cc3b7acd2774
Mohit Nakrani <monakran@microsoft.com>
2020-06-01 14:05:05 -0700

    update version

--- wvd-templates/wvd-scaling-script/ARM_based/basicScale.ps1
+++ wvd-templates/wvd-scaling-script/ARM_based/basicScale.ps1
@@ -1,7 +1,7 @@
 ﻿
 <#
 .SYNOPSIS
-	v0.1.7
+	v0.1.8
 .DESCRIPTION
 	# //todo add stuff from https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1
 #>
