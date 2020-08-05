0716640334c89f1b26e70c965c8d6c6596942201
Mohit Nakrani <nakranimohit0@users.noreply.github.com>
2020-08-05 16:09:37 -0700

    increment version of CreateOrUpdateAzAutoAccount.ps1

--- wvd-templates/wvd-scaling-script/CreateOrUpdateAzAutoAccount.ps1
+++ wvd-templates/wvd-scaling-script/CreateOrUpdateAzAutoAccount.ps1
@@ -2,7 +2,7 @@
 <#
 .SYNOPSIS
 	This is a sample script to deploy the required resources to execute scaling script in Microsoft Azure Automation Account.
-	v0.1.5
+	v0.1.6
 	# //todo refactor stuff from https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comment_based_help?view=powershell-5.1
 #>
 param(
