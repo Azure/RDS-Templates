6fec073408d8d08ece764d2532496b59fd242592
Igor Pagliai <igorpag@microsoft.com>
2022-07-15 10:34:36 +0200

    Added files for Azure Firewall Policy

---
+++ AzureFirewallPolicyForAVD/Deploy_PowerShell.ps1
@@ -0,0 +1,24 @@
+Connect-AzAccount
+Select-AzSubscription -Subscription "<<<Your Subscription ID >>>"
+
+# Variable definition
+$ResourceGrupName = "<<<Your Resource Group Name>>>"
+$Location = "<<<Your Azure Region>>>"
+
+# Run the deployment
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGrupName -Location $Location -TemplateFile ".\FirewallPolicyForAVD-template.json" -TemplateParameterFile ".\FirewallPolicyForAVD-parameters-.json"
+
+# Once completed, review all the Policy settings and rules, then associate to an existing Firewall: #
+
+$fwpolicyname = "<<<Your AVD Firewall Policy Name>>>"
+$fwpolicyresourcegroup = "<<<Resource Group where the Policy has been created>>>"
+$fwname = "<<<Your Firewall Name>>>"
+$fwresourcegroup = "<<<Resource Group where the Azure Firewall is located>>>"
+
+$azFw = Get-AzFirewall -Name $fwname -ResourceGroupName $fwresourcegroup
+$azPolicy = Get-AzFirewallPolicy -Name $fwpolicyname -ResourceGroupName $fwpolicyresourcegroup
+
+$azFw.FirewallPolicy = $azPolicy.Id
+$azFw | Set-AzFirewall
+
+
