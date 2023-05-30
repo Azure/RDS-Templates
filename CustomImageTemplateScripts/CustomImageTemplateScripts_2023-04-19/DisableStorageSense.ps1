cf8ff5ceae311c9ffdae9c16ed89a65545c633f2
Marcus Ferreira <marcus.ferreira@microsoft.com>
2023-05-30 12:24:05 -0300

    Update DisableStorageSense.ps1
    Move function Set-RegKey to top of the script as it was being called before the function was declared.

--- CustomImageTemplateScripts/CustomImageTemplateScripts_2023-04-19/DisableStorageSense.ps1
+++ CustomImageTemplateScripts/CustomImageTemplateScripts_2023-04-19/DisableStorageSense.ps1
@@ -6,6 +6,16 @@
 #    Disable Storage Sense            #
 #######################################
 
+function Set-RegKey($registryPath, $registryKey, $registryValue) {
+    try {
+        Write-Host "*** AVD AIB CUSTOMIZER PHASE ***  Disable Storage Sense - Setting  $registryKey with value $registryValue ***"
+        New-ItemProperty -Path $registryPath -Name $registryKey -Value $registryValue -PropertyType DWORD -Force -ErrorAction Stop
+    }
+    catch {
+        Write-Host "*** AVD AIB CUSTOMIZER PHASE ***   Disable Storage Sense  - Cannot add the registry key  $registryKey *** : [$($_.Exception.Message)]"
+    }
+}
+
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 Write-Host "***Starting AVD AIB CUSTOMIZER PHASE: Disable Storage Sense Start -  $((Get-Date).ToUniversalTime()) "
 
@@ -15,11 +25,11 @@ $registryValue = "0"
 
 $registryPathWin11 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\StorageSense"
 
-IF(!(Test-Path $registryPath)) {
+IF (!(Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force
 }
 
-IF(!(Test-Path $registryPathWin11)) {
+IF (!(Test-Path $registryPathWin11)) {
     New-Item -Path $registryPathWin11 -Force
 }
 
@@ -30,16 +40,6 @@ $stopwatch.Stop()
 $elapsedTime = $stopwatch.Elapsed
 Write-Host "*** AVD AIB CUSTOMIZER PHASE: Disable Storage Sense - Exit Code: $LASTEXITCODE ***"
 Write-Host "*** Ending AVD AIB CUSTOMIZER PHASE: Disable Storage Sense - Time taken: $elapsedTime "
-
-function Set-RegKey($registryPath, $registryKey, $registryValue) {
-    try {
-         Write-Host "*** AVD AIB CUSTOMIZER PHASE ***  Disable Storage Sense - Setting  $registryKey with value $registryValue ***"
-         New-ItemProperty -Path $registryPath -Name $registryKey -Value $registryValue -PropertyType DWORD -Force -ErrorAction Stop
-    }
-    catch {
-         Write-Host "*** AVD AIB CUSTOMIZER PHASE ***   Disable Storage Sense  - Cannot add the registry key  $registryKey *** : [$($_.Exception.Message)]"
-    }
- }
 
 #############
 #    END    #
