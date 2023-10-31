516bdbc8100335050142389da563ba37f67d1f20
Scott Hanberg <schanber@microsoft.com>
2023-10-31 08:10:56 -0700

    revert configuration files

--- ARM-wvd-templates/DSC/Configuration/AvdFunctions.ps1
+++
@@ -1,9 +0,0 @@
-function GetAvdSessionHostName {
-    $Wmi = (Get-WmiObject win32_computersystem)
-    
-    if ($Wmi.Domain -eq "WORKGROUP") {
-        return "$($Wmi.DNSHostName)"
-    }
-
-    return "$($Wmi.DNSHostName).$($Wmi.Domain)"
-}
